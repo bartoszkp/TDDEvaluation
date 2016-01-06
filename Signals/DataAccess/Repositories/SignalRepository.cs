d9c6af9f3076fde41a7873f87e1107637b12bb12
bartoszkp <bartoszkp@gmail.com>
2016-01-07 00:29:11 +0100

    Added DB repository and NHibernate draft (not used yet)

---
+++ Signals/DataAccess/Repositories/SignalRepository.cs
@@ -0,0 +1,36 @@
+﻿using Domain;
+
+namespace DataAccess.Repositories
+{
+    public class SignalRepository : RepositoryBase, Domain.Repositories.ISignalRepository
+    {
+        public SignalRepository(ISessionProvider sessionProvider)
+            : base(sessionProvider)
+        {
+        }
+
+        public Signal Add(Signal signal)
+        {
+            Session.SaveOrUpdate(signal);
+
+            return signal;
+        }
+
+        public Signal Get(Path path)
+        {
+            var pathString = path.ToString();
+
+            return Session
+                .QueryOver<Signal>()
+                .Where(s => s.Path.ToString() == pathString)
+                .SingleOrDefault();
+        }
+
+        public void Remove(Path path)
+        {
+            var signal = Get(path);
+
+            Session.Delete(signal);
+        }
+    }
+}
---
+++ Signals/DataAccess/UnitOfWorkProvider.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg;
+using NHibernate;
+
+namespace DataAccess
+{
+    public class UnitOfWorkProvider : ISessionProvider, IDisposable
+    {
+        private readonly ISessionFactory sessionFactory;
+
+        private ThreadLocal<ISession> sessionLocal = new ThreadLocal<ISession>(() => null);
+
+        public UnitOfWorkProvider()
+        {
+            this.sessionFactory = CreateSessionFactory();
+        }
+
+        private ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+                .Database(FluentNHibernate.Cfg.Db.MsSqlConfiguration.MsSql2012.ConnectionString(
+                    b => b.FromConnectionStringWithKey("signals")))
+                .Mappings(m => m.AutoMappings.Add(AutoMap.AssemblyOf<Domain.Signal>()))
+                .BuildSessionFactory();
+        }
+
+        public ISession Session
+        {
+            get { return sessionLocal.Value; }
+        }
+
+        private UnitOfWork OpenUnitOfWork(bool readOnly)
+        {
+            if (sessionLocal.Value == null)
+            {
+                sessionLocal.Value = sessionFactory.OpenSession();
+                var unitOfWork = new UnitOfWork(sessionLocal.Value, readOnly);
+                unitOfWork.Closed += new EventHandler(OnUnitOfWorkClosed);
+                return unitOfWork;
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot open new unit of work when previous unit of work for this thread has not been closed.");
+            }
+        }
+
+        public UnitOfWork OpenReadOnlyUnitOfWork()
+        {
+            return OpenUnitOfWork(true);
+        }
+
+        public UnitOfWork OpenUnitOfWork()
+        {
+            return OpenUnitOfWork(false);
+        }
+
+        private void OnUnitOfWorkClosed(object sender, EventArgs e)
+        {
+            UnitOfWork unitOfWork = (UnitOfWork)sender;
+            unitOfWork.Closed -= OnUnitOfWorkClosed;
+
+            sessionLocal.Value = null;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                sessionLocal.Dispose();
+                sessionFactory.Dispose();
+            }
+        }
+    }
+}
