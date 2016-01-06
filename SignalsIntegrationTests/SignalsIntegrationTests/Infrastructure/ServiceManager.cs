7b1ae4ac4cfdc4b659cabb2f10ef6bd48be0f16a
bartoszkp <bartoszkp@gmail.com>
2016-01-06 15:26:01 +0100

    Added service manager so tests can automatically start and stop the SUT

---
+++ SignalsIntegrationTests/SignalsIntegrationTests/Infrastructure/ServiceManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace SignalsIntegrationTests.Infrastructure
+{
+    public class ServiceManager
+    {
+        private const string ExecutableHostPathSettingName = "SignalsHostExecutablePath";
+
+        private Process serviceProcess = null;
+
+        public void StartService()
+        {
+            if (serviceProcess != null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            FileInfo executableFileInfo = new FileInfo(Properties.Settings.Default[ExecutableHostPathSettingName] as string);
+
+            if (!executableFileInfo.Exists)
+            {
+                throw new InvalidOperationException(
+                    "Executable host: '"
+                    + executableFileInfo.FullName
+                    + "' does not exist. Please set the Visual Studio Project's setting '"
+                    + ExecutableHostPathSettingName
+                    + "' to contain full path to the correct executable file");
+            }
+
+            ProcessStartInfo psi = new ProcessStartInfo()
+            {
+                ErrorDialog = false,
+                RedirectStandardInput = true,
+                FileName = executableFileInfo.FullName,
+                WorkingDirectory = executableFileInfo.DirectoryName,
+                Verb = "runas",
+                UseShellExecute = false
+            };
+
+            serviceProcess = Process.Start(psi);
+        }
+
+        public void StopService()
+        {
+            if (serviceProcess == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            serviceProcess.StandardInput.WriteLine();
+
+            var finished = serviceProcess.WaitForExit(100);
+
+            if (!finished)
+            {
+                serviceProcess.Kill();
+            }
+
+            serviceProcess = null;
+        }
+    }
+}
---
+++ SignalsIntegrationTests/SignalsIntegrationTests/ServiceManagerExample.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SignalsIntegrationTests.Infrastructure;
+
+namespace SignalsIntegrationTests
+{
+    [TestClass]
+    public class ServiceManagerExample
+    {
+        private WS.SignalsClient signalsClient;
+        private static ServiceManager serviceManager;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            serviceManager = new ServiceManager();
+            serviceManager.StartService();
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        { 
+            signalsClient = new WS.SignalsClient();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            serviceManager.StopService();
+        }
+
+        [TestMethod]
+        public void DummyTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
