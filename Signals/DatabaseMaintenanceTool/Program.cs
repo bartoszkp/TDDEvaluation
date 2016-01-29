a47f55f9d001b68cd495689b678b893265563242
Konrad Grochowski <K.Grochowski@ii.pw.edu.pl>
2016-01-29 18:00:00 +0100

    Application configurations now use relative paths in connection strings

--- Signals/DatabaseMaintenanceTool/Program.cs
+++ Signals/DatabaseMaintenanceTool/Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.IO;
 
 namespace DatabaseMaintenanceTool
 {
     class Program
     {
+        private static void setupDataDirectory()
+        {
+            var relativeDataDirectory = Properties.Settings.Default["RelativeDataDirectory"] as string;
+            var absoluteDataDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativeDataDirectory));
+            AppDomain.CurrentDomain.SetData("DataDirectory", absoluteDataDirectory);
+        }
+
         static void Main(string[] args)
         {
             if (args.Length == 0)
@@ -15,6 +23,8 @@ namespace DatabaseMaintenanceTool
             if (args[0] == "rebuild")
             {
                 Console.WriteLine("Rebuilding database...");
+
+                setupDataDirectory();
 
                 DatabaseMaintenance.DatabaseMaintenance dm = new DatabaseMaintenance.DatabaseMaintenance(new DataAccess.UnitOfWorkProvider());
 
