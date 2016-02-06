4765b9eb443f1678fb2b86c05eac17b5eac6d2e4
bartoszkp <bartoszkp@gmail.com>
2016-02-06 01:55:03 +0100

    GranuralityUtils refactoring

--- Signals/Domain/Infrastructure/GranularityUtils.cs
+++ Signals/Domain/Infrastructure/GranularityUtils.cs
@@ -1,96 +1,74 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace Domain.Infrastructure
 {
     public static class GranularityUtils
     {
-        private static void requireZeroMilliseconds(DateTime timestamp)
-        {
-            if (timestamp.Millisecond != 0)
-                throw new ArgumentException("Non zero Millisecond value", "timestamp");
-        }
-
-        private static void requireZeroSeconds(DateTime timestamp)
-        {
-            if (timestamp.Second != 0)
-                throw new ArgumentException("Non zero Second value", "timestamp");
-        }
-
-        private static void requireZeroMinutes(DateTime timestamp)
-        {
-            if (timestamp.Minute!= 0)
-                throw new ArgumentException("Non zero Minute value", "timestamp");
-        }
-
-        private static void requireZeroHours(DateTime timestamp)
-        {
-            if (timestamp.Hour != 0)
-                throw new ArgumentException("Non zero Hour value", "timestamp");
-        }
-
-        private static void requireMonday(DateTime timestamp)
-        {
-            if (timestamp.DayOfWeek != DayOfWeek.Monday)
-                throw new ArgumentException("Non Monday in DayOfWeek", "timestamp");
-        }
-
-        private static void requireFirstDayOfMonth(DateTime timestamp)
-        {
-            if (timestamp.Day != 1)
-                throw new ArgumentException("Not first Day of Month", "timestamp");
-        }
-
-        private static void requireFirstMonth(DateTime timestamp)
-        {
-            if (timestamp.Month != 1)
-                throw new ArgumentException("Not first Month", "timestamp");
-        }
-
         public static void ValidateTimestamp(this Granularity @this, DateTime timestamp)
         {
             switch (@this)
             {
                 case Granularity.Second:
-                    requireZeroMilliseconds(timestamp);
+                    RequireZero(timestamp, ts => ts.Millisecond);
                     break;
                 case Granularity.Minute:
-                    requireZeroMilliseconds(timestamp);
-                    requireZeroSeconds(timestamp);
+                    RequireAllZero(timestamp, ts => ts.Millisecond, ts => ts.Second);
                     break;
                 case Granularity.Hour:
-                    requireZeroMilliseconds(timestamp);
-                    requireZeroSeconds(timestamp);
-                    requireZeroMinutes(timestamp);
+                    RequireAllZero(timestamp, ts => ts.Millisecond, ts => ts.Second, ts => ts.Minute);
                     break;
                 case Granularity.Day:
-                    requireZeroMilliseconds(timestamp);
-                    requireZeroSeconds(timestamp);
-                    requireZeroMinutes(timestamp);
-                    requireZeroHours(timestamp);
+                    RequireAllZero(timestamp, ts => ts.Millisecond, ts => ts.Second, ts => ts.Minute, ts => ts.Hour);
                     break;
                 case Granularity.Week:
-                    requireZeroMilliseconds(timestamp);
-                    requireZeroSeconds(timestamp);
-                    requireZeroMinutes(timestamp);
-                    requireZeroHours(timestamp);
-                    requireMonday(timestamp);
+                    RequireAllZero(timestamp, ts => ts.Millisecond, ts => ts.Second, ts => ts.Minute, ts => ts.Hour);
+                    RequireMonday(timestamp);
                     break;
                 case Granularity.Month:
-                    requireZeroMilliseconds(timestamp);
-                    requireZeroSeconds(timestamp);
-                    requireZeroMinutes(timestamp);
-                    requireZeroHours(timestamp);
-                    requireFirstDayOfMonth(timestamp);
+                    RequireAllZero(timestamp, ts => ts.Millisecond, ts => ts.Second, ts => ts.Minute, ts => ts.Hour);
+                    RequireFirstDayOfMonth(timestamp);
                     break;
                 case Granularity.Year:
-                    requireZeroMilliseconds(timestamp);
-                    requireZeroSeconds(timestamp);
-                    requireZeroMinutes(timestamp);
-                    requireZeroHours(timestamp);
-                    requireFirstDayOfMonth(timestamp);
-                    requireFirstMonth(timestamp);
+                    RequireAllZero(timestamp, ts => ts.Millisecond, ts => ts.Second, ts => ts.Minute, ts => ts.Hour);
+                    RequireFirstDayOfMonth(timestamp);
+                    RequireFirstMonth(timestamp);
                     break;
             }
+        }
+
+        private static void RequireAllZero(DateTime timestamp, params Expression<Func<DateTime, int>>[] expressions)
+        {
+            foreach (var expression in expressions)
+            {
+                RequireZero(timestamp, expression);
+            }
+        }
+
+        private static void RequireZero(DateTime timestamp, Expression<Func<DateTime, int>> expression)
+        {
+            var compiled = expression.Compile();
+
+            if (compiled(timestamp) != 0)
+                throw new ArgumentException(string.Format("Non zero DateTime property: {0}.", ReflectionUtils.GetMemberInfo(expression).Name), "timestamp");
+        }
+
+        private static void RequireMonday(DateTime timestamp)
+        {
+            if (timestamp.DayOfWeek != DayOfWeek.Monday)
+                throw new ArgumentException("Non Monday in DayOfWeek", "timestamp");
+        }
+
+        private static void RequireFirstDayOfMonth(DateTime timestamp)
+        {
+            if (timestamp.Day != 1)
+                throw new ArgumentException("Not first Day of Month", "timestamp");
+        }
+
+        private static void RequireFirstMonth(DateTime timestamp)
+        {
+            if (timestamp.Month != 1)
+                throw new ArgumentException("Not first Month", "timestamp");
         }
     }
 }
