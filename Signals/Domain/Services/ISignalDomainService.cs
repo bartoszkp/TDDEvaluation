7bc1955a5ee7f36aec82110d82b987edbb878d9a
bartoszkp <bartoszkp@gmail.com>
2016-02-06 01:18:06 +0100

    Unified naming - all plural (all singular was too much work)

---
+++ Signals/Domain/Services/ISignalDomainService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Services
+{
+    public interface ISignalDomainService
+    {
+        Signal Get(Path path);
+
+        Signal Add(Signal signal);
+
+        IEnumerable<Datum<T>> GetData<T>(Signal signal, DateTime fromIncluded, DateTime toExcluded);
+
+        void SetData<T>(Signal signal, IEnumerable<Datum<T>> data);
+    }
+}
