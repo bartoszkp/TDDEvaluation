59f251e5dc8b31019a08d6d475c009522d781322
Konrad Grochowski <K.Grochowski@ii.pw.edu.pl>
2016-02-11 15:31:11 +0100

    SignalPathGenerator extracted

---
+++ SignalsIntegrationTests/SignalsIntegrationTests/Infrastructure/SignalPathGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SignalsIntegrationTests.Infrastructure
+{
+    public class SignalPathGenerator
+    {
+        private static int signalCounter = 0;
+
+        public static Path Generate()
+        {
+            Interlocked.Increment(ref signalCounter);
+            return Path.FromString("/new/signal" + signalCounter.ToString());
+        }
+
+        private SignalPathGenerator()
+        {
+
+        }
+    }
+}
