7fbf8395fdbd2856ba08687dde9d541c419f96f6
bartoszkp <bartoszkp@gmail.com>
2016-05-11 22:29:32 +0200

    Moved MissingValuePolicy DAL operations to a separate repository

---
+++ Signals/DataAccess/Repositories/MissingValuePolicyRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.MissingValuePolicy;
+
+namespace DataAccess.Repositories
+{
+    public class MissingValuePolicyRepository : RepositoryBase, Domain.Repositories.IMissingValuePolicyRepository
+    {
+        public MissingValuePolicyRepository(ISessionProvider sessionProvider)
+            : base(sessionProvider)
+        {
+        }
+
+        public void Delete(MissingValuePolicy missingValuePolicy)
+        {
+            Session.Delete(missingValuePolicy);
+        }
+    }
+}
--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -10,10 +10,12 @@ namespace Domain.Services.Implementation
     public class SignalsDomainService : ISignalsDomainService
     {
         private readonly ISignalsRepository signalRepository;
+        private readonly IMissingValuePolicyRepository missingValuePolicyRepository;
 
-        public SignalsDomainService(ISignalsRepository signalRepository)
+        public SignalsDomainService(ISignalsRepository signalRepository, IMissingValuePolicyRepository missingValuePolicyRepository)
         {
             this.signalRepository = signalRepository;
+            this.missingValuePolicyRepository = missingValuePolicyRepository;
         }
 
         public Signal Get(Path path)
@@ -103,7 +105,7 @@ namespace Domain.Services.Implementation
         {
             if (signal.MissingValuePolicy != null)
             {
-                this.signalRepository.Delete(signal.MissingValuePolicy);
+                this.missingValuePolicyRepository.Delete(signal.MissingValuePolicy);
             }
 
             signal = this.signalRepository.Get(signal.Path);
