a7c846427d38a39145ad173912f33787cdd851c1
bartoszkp <bartoszkp@gmail.com>
2016-06-02 19:16:16 +0200

    Shortened Datum<T> comparer creation

--- SignalsIntegrationTests/SignalsIntegrationTests/Infrastructure/MissingValuePolicyTestsBase.cs
+++ SignalsIntegrationTests/SignalsIntegrationTests/Infrastructure/MissingValuePolicyTestsBase.cs
@@ -22,27 +22,12 @@ namespace SignalsIntegrationTests.Infrastructure
             TestsBase.ClassCleanup();
         }
 
-        private class DatumEqualityComparer<T> : IComparer, IComparer<Datum<T>>
-        {
-            public int Compare(object x, object y)
-            {
-                var lhs = x as Datum<T>;
-                var rhs = y as Datum<T>;
-                if (lhs == null || rhs == null) throw new InvalidOperationException();
-                return Compare(lhs, rhs);
-            }
-
-            public int Compare(Datum<T> x, Datum<T> y)
-            {
-                if (x.Value.Equals(y.Value) && x.Timestamp.Equals(y.Timestamp))
-                    return 0;
-                return 1;
-            }
-        }
-
         private void AssertDatumsEqual<T>(IEnumerable<Datum<T>> expected, IEnumerable<Datum<T>> actual)
         {
-            CollectionAssert.AreEqual(expected.ToList(), actual.ToList(), new DatumEqualityComparer<T>());
+            CollectionAssert.AreEqual(
+                expected.ToList(), 
+                actual.ToList(), 
+                Comparer<Datum<T>>.Create((x, y) => x.Value.Equals(y.Value) && x.Timestamp.Equals(y.Timestamp) ? 0 : 1));
         }
 
         protected class MissingValuePolicyValidator
