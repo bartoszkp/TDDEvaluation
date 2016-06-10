e7fbf52efb0dd7dee30681b24aef0328b9f9acd2
bartoszkp <bartoszkp@gmail.com>
2016-06-11 01:13:57 +0200

    Removed unnecessary IDictionary mapping configuration after Mapster update

--- Signals/Dto/Conversions/AutoMappingConfiguration.cs
+++ Signals/Dto/Conversions/AutoMappingConfiguration.cs
@@ -1,16 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Mapster;
-
-namespace Dto.Conversions
+﻿namespace Dto.Conversions
 {
     public static class AutoMappingConfiguration
     {
         public static void Run()
         {
-            TypeAdapterConfig<IDictionary<string, object>, IDictionary<string, object>>
-                .NewConfig()
-                .MapWith(dictionary => dictionary.ToDictionary(kvp => kvp.Key, kvp => kvp.Value));
         }
     }
 }
