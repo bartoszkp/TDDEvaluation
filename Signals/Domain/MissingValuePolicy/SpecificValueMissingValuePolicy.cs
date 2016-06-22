375b42ecde35d94f8a2c942e8909359138a968ba
Konrad Grochowski <K.Grochowski@ii.pw.edu.pl>
2016-06-22 16:51:05 +0200

    SpecificValueMissingValuePolicy first impl

--- Signals/Domain/MissingValuePolicy/SpecificValueMissingValuePolicy.cs
+++ Signals/Domain/MissingValuePolicy/SpecificValueMissingValuePolicy.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Domain.MissingValuePolicy
 {
@@ -12,7 +13,11 @@ namespace Domain.MissingValuePolicy
 
         public override IEnumerable<Datum<T>> FillMissingData(TimeEnumerator timeEnumerator, IEnumerable<Datum<T>> readData)
         {
-            throw new NotImplementedException();
+            var readDataDict = readData.ToDictionary(d => d.Timestamp, d => d);
+
+            return timeEnumerator
+                .Select(ts => readDataDict.ContainsKey(ts) ? readDataDict[ts] : new Datum<T>() { Value = Value, Quality = Quality, Signal = Signal, Timestamp = ts })
+                .ToArray();
         }
     }
 }
