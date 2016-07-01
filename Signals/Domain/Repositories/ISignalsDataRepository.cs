8a8a975d0072d7b97a6d7d25906d4f5923e5da4f
bartoszkp <bartoszkp@gmail.com>
2016-07-01 16:30:34 +0200

    Separated signal's data handling from signal handling

---
+++ Signals/Domain/Repositories/ISignalsDataRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Repositories
+{
+    public interface ISignalsDataRepository
+    {
+        IEnumerable<Datum<T>> GetData<T>(Signal signal, DateTime fromIncludedUtc, DateTime toExcludedUtc);
+
+        void SetData<T>(IEnumerable<Datum<T>> data);
+    }
+}
