e80f6e63af187a9556eb20ac2c446db65a9a6026
Konrad Grochowski <K.Grochowski@ii.pw.edu.pl>
2016-07-04 23:09:27 +0200

    SignalDataTests extracted from BasicSignalTests

--- SignalsIntegrationTests/SignalsIntegrationTests/BasicSignalTests.cs
+++ SignalsIntegrationTests/SignalsIntegrationTests/BasicSignalTests.cs
@@ -1,5 +1,4 @@
 using System;
-using System.ServiceModel;
 using Domain;
 using Dto.Conversions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -107,116 +106,6 @@ namespace SignalsIntegrationTests
         }
 
         [TestMethod]
-        public void CanWriteAndRetrieveData()
-        {
-            var path = SignalPathGenerator.Generate();
-            var timestamp = new DateTime(2019, 4, 14);
-
-            var newSignal1 = new Signal()
-            {
-                Path = path,
-                Granularity = Granularity.Day,
-                DataType = DataType.Integer
-            };
-
-            var signal = client.Add(newSignal1.ToDto<Dto.Signal>());
-
-            var data = new[]
-            {
-                new Datum<int>()
-                {
-                    Timestamp = timestamp,
-                    Value = 4,
-                    Quality = Quality.Fair
-                }
-            };
-
-            client.SetData(signal.Id.Value, data.ToDto<Dto.Datum[]>());
-            var retrievedData = client.GetData(signal.Id.Value, timestamp, timestamp.AddDays(1));
-
-            Assert.AreEqual(data.Length, retrievedData.Length);
-            Assert.AreEqual(data[0].Value, retrievedData[0].Value);
-            Assert.AreEqual(data[0].Timestamp, retrievedData[0].Timestamp);
-            Assert.AreEqual(data[0].Quality, retrievedData[0].ToDomain<Domain.Datum<int>>().Quality);
-        }
-
-        [TestMethod]
-        public void CanWriteAndRetrieveMonthlyData()
-        {
-            var path = SignalPathGenerator.Generate();
-            var timestamp = new DateTime(2019, 4, 1);
-
-            var newSignal1 = new Signal()
-            {
-                Path = path,
-                Granularity = Granularity.Month,
-                DataType = DataType.Integer
-            };
-
-            var signal = client.Add(newSignal1.ToDto<Dto.Signal>());
-
-            var data = new[]
-            {
-                new Datum<int>()
-                {
-                    Timestamp = timestamp,
-                    Value = 4,
-                    Quality = Quality.Fair
-                }
-            };
-
-            client.SetData(signal.Id.Value, data.ToDto<Dto.Datum[]>());
-            var retrievedData = client.GetData(signal.Id.Value, timestamp, timestamp.AddDays(1));
-
-            Assert.AreEqual(data.Length, retrievedData.Length);
-            Assert.AreEqual(data[0].Value, retrievedData[0].Value);
-            Assert.AreEqual(data[0].Timestamp, retrievedData[0].Timestamp);
-            Assert.AreEqual(data[0].Quality, retrievedData[0].ToDomain<Domain.Datum<int>>().Quality);
-        }
-
-        [TestMethod]
-        public void CanWriteAndRetrieveYearlyData()
-        {
-            var path = SignalPathGenerator.Generate();
-            var timestamp = new DateTime(2019, 1, 1);
-
-            var newSignal1 = new Signal()
-            {
-                Path = path,
-                Granularity = Granularity.Year,
-                DataType = DataType.Integer
-            };
-
-            var signal = client.Add(newSignal1.ToDto<Dto.Signal>());
-
-            var data = new[]
-            {
-                new Datum<int>()
-                {
-                    Timestamp = timestamp,
-                    Value = 4,
-                    Quality = Quality.Fair
-                }
-            };
-
-            client.SetData(signal.Id.Value, data.ToDto<Dto.Datum[]>());
-            var retrievedData = client.GetData(signal.Id.Value, timestamp, timestamp.AddDays(1));
-
-            Assert.AreEqual(data.Length, retrievedData.Length);
-            Assert.AreEqual(data[0].Value, retrievedData[0].Value);
-            Assert.AreEqual(data[0].Timestamp, retrievedData[0].Timestamp);
-            Assert.AreEqual(data[0].Quality, retrievedData[0].ToDomain<Domain.Datum<int>>().Quality);
-        }
-
-        [TestMethod]
-        public void GetDataUsingIncompleteSignalsThrowsOrReturnsNull()
-        {
-            int dummySignalId = 0;
-
-            Assertions.AssertReturnsNullOrThrows(() => client.GetData(dummySignalId, new DateTime(2016, 12, 10), new DateTime(2016, 12, 14)));
-        }
-
-        [TestMethod]
         public void TryingToAddSignalWithExistingPathThrowsOrReturnsNull()
         {
             var signal = new Signal()
@@ -243,24 +132,6 @@ namespace SignalsIntegrationTests
             };
 
             Assertions.AssertReturnsNullOrThrows(() => client.Add(signal.ToDto<Dto.Signal>()));
-        }
-
-        [TestMethod]
-        public void SignalWithoutDataReturnsNoneQualityDatumsForEachTimerangeStep()
-        {
-            var signal = AddNewIntegerSignal(Granularity.Day);
-
-            const int numberOfDays = 5;
-            var timestamp = new DateTime(2019, 1, 1);
-            var receivedData = client.GetData(signal.Id.Value, timestamp, timestamp.AddDays(numberOfDays));
-
-            Assert.AreEqual(numberOfDays, receivedData.Length);
-            foreach (var datum in receivedData)
-            {
-                Assert.AreEqual(timestamp, datum.Timestamp);
-                Assert.AreEqual(Dto.Quality.None, datum.Quality);
-                timestamp = timestamp.AddDays(1);
-            }
         }
 
         [TestMethod]
@@ -301,36 +172,7 @@ namespace SignalsIntegrationTests
             Assert.AreEqual(Quality.Fair, specificMissingValuePolicy.Quality);
         }
 
-        [TestMethod]
-        public void WhenDeletingNonExistentSignal_Throws()
-        {
-            Assertions.AssertThrows(() => client.Delete(0));
-        }
-
-        [TestMethod]
-        public void WhenDeletingExistingSignal_SignalDisappears()
-        {
-            var signalId = AddNewIntegerSignal(granularity: Granularity.Year).Id.Value;
-
-            client.Delete(signalId);
-
-            Assertions.AssertThrows(() => client.GetById(signalId));
-        }
-
-        [TestMethod]
-        public void WhenDeletingExistingSignalWithData_BothSignalAndDataDisappear()
-        {
-            var signalId = AddNewIntegerSignal(granularity: Granularity.Year).Id.Value;
-
-            var ts = new DateTime(2000, 1, 1);
-
-            client.SetData(signalId, new[] { new Dto.Datum() { Quality = Dto.Quality.Good, Timestamp = ts, Value = 0 } });
-
-            client.Delete(signalId);
-
-            Assertions.AssertThrows(() => client.GetData(signalId, ts, ts.AddYears(1)));
-        }
-
+        // TODO removing?
         // TODO editing?
         // TODO changing path?
 
