3a4b15174703ad42d88b667bb49bc472159567d5
? <?>
2016-08-01 01:15:54 +0200

    initial commit

---
+++ Signals/Domain/Infrastructure/NoInjectionMembers.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Practices.Unity;
+
+namespace Domain.Infrastructure
+{
+    public class NoInjectionMembers : IInjectionMembersFactory
+    {
+        public IEnumerable<InjectionMember> GetInjectionMembers()
+        {
+            return Enumerable.Empty<InjectionMember>();
+        }
+    }
+}
---
+++ Signals/Dto/Signal.cs
@@ -0,0 +1,13 @@
+﻿namespace Dto
+{
+    public class Signal
+    {
+        public int? Id { get; set; }
+
+        public DataType DataType { get; set; }
+
+        public Granularity Granularity { get; set; }
+
+        public Path Path { get; set; }
+    }
+}
