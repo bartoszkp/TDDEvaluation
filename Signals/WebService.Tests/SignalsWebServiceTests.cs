e809709c4707aafade7a5701bec8e293d573851f
bartoszkp <bartoszkp@gmail.com>
2016-08-01 01:26:32 +0200

    GivenNoSignals_WhenAddingASignal_ReturnsTheSameSignalExceptForId fails

--- Signals/WebService.Tests/SignalsWebServiceTests.cs
+++ Signals/WebService.Tests/SignalsWebServiceTests.cs
@@ -22,6 +22,23 @@ namespace WebService.Tests
 
                 Assert.IsNotNull(result);
             }
+
+            [TestMethod]
+            public void GivenNoSignals_WhenAddingASignal_ReturnsTheSameSignalExceptForId()
+            {
+                var signalsWebService = new SignalsWebService(null);
+
+                var result = signalsWebService.Add(new Dto.Signal()
+                {
+                    DataType = Dto.DataType.Decimal,
+                    Granularity = Dto.Granularity.Week,
+                    Path = new Dto.Path() { Components = new[] { "root", "signal" } }
+                });
+
+                Assert.AreEqual(Dto.DataType.Decimal, result.DataType);
+                Assert.AreEqual(Dto.Granularity.Week, result.Granularity);
+                CollectionAssert.AreEqual(new[] { "root", "signal" }, result.Path.Components.ToArray());
+            }
         }
     }
 }
