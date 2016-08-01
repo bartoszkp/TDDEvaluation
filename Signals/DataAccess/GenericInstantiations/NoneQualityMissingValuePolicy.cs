0f69dc2c52e0f08ce65dabba731d8e5c6ee09bc3
? <?>
2016-08-01 02:22:45 +0200

    initial commit

---
+++ Signals/DataAccess/GenericInstantiations/NoneQualityMissingValuePolicy.cs
@@ -0,0 +1,22 @@
+﻿namespace DataAccess.GenericInstantiations
+{
+    public class NoneQualityMissingValuePolicyBoolean : Domain.MissingValuePolicy.NoneQualityMissingValuePolicy<bool>
+    {
+    }
+
+    public class NoneQualityMissingValuePolicyInteger : Domain.MissingValuePolicy.NoneQualityMissingValuePolicy<int>
+    {
+    }
+
+    public class NoneQualityMissingValuePolicyDouble : Domain.MissingValuePolicy.NoneQualityMissingValuePolicy<double>
+    {
+    }
+
+    public class NoneQualityMissingValuePolicyDecimal : Domain.MissingValuePolicy.NoneQualityMissingValuePolicy<decimal>
+    {
+    }
+
+    public class NoneQualityMissingValuePolicyString : Domain.MissingValuePolicy.NoneQualityMissingValuePolicy<string>
+    {
+    }
+}
