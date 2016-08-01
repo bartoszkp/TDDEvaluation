57aa58fe93a6be0071dd368688df7725a850a210
? <?>
2016-08-01 02:22:45 +0200

    initial commit

---
+++ Signals/Domain/Datum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Datum<T>
+    {
+        public static Datum<T> CreateNone(Signal signal, DateTime timestamp)
+        {
+            return new Datum<T>()
+            {
+                Id = 0,
+                Signal = signal,
+                Timestamp = timestamp,
+                Value = default(T),
+                Quality = Quality.None
+            };
+        }
+
+        public virtual int Id { get; set; }
+
+        public virtual Signal Signal { get; set; }
+
+        public virtual DateTime Timestamp { get; set; }
+
+        public virtual T Value { get; set; }
+
+        public virtual Quality Quality { get; set; }
+    }
+}
---
+++ Signals/Domain/Infrastructure/UnityRegisterAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Practices.Unity;
+
+namespace Domain.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class UnityRegisterAttribute : Attribute
+    {
+        public LifetimeManager LifetimeManager { get; private set; }
+
+        public IEnumerable<InjectionMember> InjectionMembers { get; private set; }
+
+        public UnityRegisterAttribute()
+            : this(typeof(TransientLifetimeManager), typeof(NoInjectionMembers))
+        {
+        }
+
+        public UnityRegisterAttribute(Type lifetimeManager)
+            : this(lifetimeManager, typeof(NoInjectionMembers))
+        {
+        }
+
+        public UnityRegisterAttribute(Type lifetimeManager, Type injectionMembersFactory)
+        {
+            LifetimeManager = lifetimeManager.GetConstructor(Type.EmptyTypes).Invoke(null) as LifetimeManager;
+            InjectionMembers = (injectionMembersFactory.GetConstructor(Type.EmptyTypes).Invoke(null)
+                as IInjectionMembersFactory)
+                .GetInjectionMembers();
+        }
+    }
+}
