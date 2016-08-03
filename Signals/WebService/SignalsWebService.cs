6f4388839382bf1900374c3cb1a25b58e7d91849
? <?>
2016-08-03 11:41:37 +0200

    WhenGettingDataDoesNotThrow passes.

--- Signals/WebService/SignalsWebService.cs
+++ Signals/WebService/SignalsWebService.cs
@@ -60,7 +60,7 @@ namespace WebService
 
         public IEnumerable<Datum> GetData(int signalId, DateTime fromIncludedUtc, DateTime toExcludedUtc)
         {
-            throw new NotImplementedException();
+            return new Datum[] { };
         }
 
         public void SetData(int signalId, IEnumerable<Datum> data)
