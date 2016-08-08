c21da892fd65d4aa15f7fd78583d105726d5158b
? <?>
2016-08-08 11:20:09 +0200

    Issue "Request for Signal with bad path or id should return null" resolved

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -9,21 +9,9 @@ namespace ExampleSignalClient
         {
             SignalsWebServiceClient client = new SignalsWebServiceClient("BasicHttpBinding_ISignalsWebService");
 
-            var newSignal = new Signal()
-            {
-                DataType = DataType.Double,
-                Granularity = Granularity.Month,
-                Path = new Path() { Components = new[] { "root", "signal1" } }
-            };
+            var result = client.GetById(50);
 
-            var id = client.Add(newSignal).Id.Value;
-
-            var result = client.GetById(id);
-
-            Console.WriteLine(result.Id);
-            Console.WriteLine(result.DataType);
-            Console.WriteLine(result.Granularity);
-            Console.WriteLine(string.Join("/", result.Path.Components));
+            Console.WriteLine(result == null);
             Console.ReadKey();
         }
     }
