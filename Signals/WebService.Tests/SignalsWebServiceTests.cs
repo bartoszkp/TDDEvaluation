58ac0cc286fb1773725324e1b55145b0663aa034
? <?>
2016-08-08 13:09:36 +0200

    GivenASignal_WhenGettingStringDataForSpecificSignal_ReturnsThisData passes

--- Signals/WebService.Tests/SignalsWebServiceTests.cs
+++ Signals/WebService.Tests/SignalsWebServiceTests.cs
@@ -638,6 +638,51 @@ namespace WebService.Tests
                 }
             }
 
+            [TestMethod]
+            public void GivenASignal_WhenGettingStringDataForSpecificSignal_ReturnsThisData()
+            {
+                var existingSignal = new Domain.Signal()
+                {
+                    Id = 1,
+                    DataType = Domain.DataType.String,
+                    Granularity = Domain.Granularity.Day,
+                    Path = Domain.Path.FromString("root/signal1")
+                };
+
+                var existingDatum = new Dto.Datum[]
+                {
+                        new Dto.Datum() { Quality = Dto.Quality.Fair, Timestamp = new DateTime(2000, 1, 1), Value = (string)"tak" },
+                        new Dto.Datum() { Quality = Dto.Quality.Good, Timestamp = new DateTime(2000, 2, 1), Value = (string)"tak" },
+                        new Dto.Datum() { Quality = Dto.Quality.Poor, Timestamp = new DateTime(2000, 3, 1), Value = (string)"nie" }
+                };
+
+                signalsDataRepositoryMock = new Mock<ISignalsDataRepository>();
+
+                signalsDataRepositoryMock
+                    .Setup(sdrm => sdrm.GetData<string>(
+                        existingSignal,
+                        existingDatum.First().Timestamp,
+                        existingDatum.Last().Timestamp))
+                    .Returns(existingDatum.ToDomain<IEnumerable<Domain.Datum<string>>>());
+
+                GivenASignal(existingSignal);
+
+                var signalsDomainService = new SignalsDomainService(signalsRepositoryMock.Object, signalsDataRepositoryMock.Object, null);
+
+                signalsWebService = new SignalsWebService(signalsDomainService);
+
+                var result = signalsWebService.GetData(existingSignal.Id.Value, existingDatum.First().Timestamp, existingDatum.Last().Timestamp);
+
+                int index = 0;
+                foreach (var ed in existingDatum)
+                {
+                    Assert.AreEqual(ed.Quality, result.ElementAt(index).Quality);
+                    Assert.AreEqual(ed.Timestamp, result.ElementAt(index).Timestamp);
+                    Assert.AreEqual(ed.Value, result.ElementAt(index).Value);
+                    index++;
+                }
+            }
+
             private Dto.Signal SignalWith(
                 int? id = null,
                 Dto.DataType dataType = Dto.DataType.Boolean,
