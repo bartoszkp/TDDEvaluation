1d3c78919d0088b7c9603249956253071f7003dc
? <?>
2016-08-09 12:09:56 +0200

    GetData_DataMissOnePoint_ShouldReturnDataWithFilledMissedPoints - not passed

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -12,15 +12,24 @@ namespace ExampleSignalClient
             var newSignal = new Signal()
             {
                 DataType = DataType.Integer,
-                Granularity = Granularity.Minute,
-                Path = new Path() { Components = new[] { "root20", "defaultPolicy" } }
+                Granularity = Granularity.Month,
+                Path = new Path() { Components = new[] { "root1", "defaultPolicy" } }
             };
 
-            var result = client.Add(newSignal);
+           var res =  client.Add(newSignal);
 
-            var mvp = client.GetMissingValuePolicy(result.Id.Value);
+            client.SetData(2, new Datum[] {
+                         new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 3, 1), Value = (int)1.5 },
+                         new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 2, 1), Value = (int)1 },
+                         new Datum() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 1, 1), Value = (int)2 },
+                        new Datum() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 4, 1), Value = (int)2 }});
 
-            Console.WriteLine(mvp);
+            var result = client.GetData(1, new DateTime(2000, 1, 1), new DateTime(2000, 4, 1));
+
+            foreach (var d in result)
+            {
+                Console.WriteLine(d.Timestamp.ToString() + ": " + d.Value.ToString() + " (" + d.Quality.ToString() + ")");
+            }
 
             Console.ReadKey();
         }
