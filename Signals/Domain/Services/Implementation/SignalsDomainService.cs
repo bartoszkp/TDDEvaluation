4083f3efcabfaae3bf6fcf8160c9c774836dfbe5
? <?>
2016-08-09 13:52:01 +0200

    FillMissingData() method implemented, it fills missing data with NoneQualityMissingValuePolicy

--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -91,8 +91,9 @@ namespace Domain.Services.Implementation
                 throw new SignalWithThisIdNonExistException();
 
             var datums = signalsDataRepository.GetData<T>(signal, fromIncludedUtc, toExcludedUtc);
-
             IEnumerable<Datum<T>> sortedDatums = datums.OrderBy(datum => datum.Timestamp);
+
+            sortedDatums = FillMissingData(signal.Granularity, sortedDatums, fromIncludedUtc, toExcludedUtc);
 
             return sortedDatums;
         }
@@ -118,6 +119,203 @@ namespace Domain.Services.Implementation
 
             IEnumerable<Datum<T>> sortedDatums = datums.OrderBy(datum => datum.Timestamp);
             signalsDataRepository.SetData<T>(sortedDatums);
+        }
+
+        private IEnumerable<Datum<T>> FillMissingData<T>(Domain.Granularity granularity, IEnumerable<Datum<T>> sortedDatums, 
+            DateTime fromIncludedUtc, DateTime toExcludedUtc)
+        {
+            List<Datum<T>> sortedDatumsList = new List<Datum<T>>();
+            sortedDatumsList = sortedDatums.ToList();
+
+            List<Datum<T>> newList = new List<Datum<T>>();
+
+            if (granularity == Granularity.Year)
+            {
+                int yearsDifference = (toExcludedUtc.Year - fromIncludedUtc.Year);
+
+                if (yearsDifference == sortedDatumsList.Count() && sortedDatumsList[0].Timestamp == fromIncludedUtc)
+                {
+                    return sortedDatums;
+                }
+                else
+                {
+                    for(int index = 0, currentYear = fromIncludedUtc.Year; currentYear < toExcludedUtc.Year;
+                        index++, currentYear++)
+                    {
+                        if (sortedDatumsList[index].Timestamp.Year != currentYear)
+                        {
+                            Datum<T> datumToAdd = new Datum<T>()
+                            {
+                                Quality = Quality.None,
+                                Value = default(T),
+                                Timestamp = fromIncludedUtc,
+                            };
+
+                            datumToAdd.Timestamp = new DateTime(currentYear, fromIncludedUtc.Month, fromIncludedUtc.Day, fromIncludedUtc.Hour, fromIncludedUtc.Minute, fromIncludedUtc.Second);
+
+                            newList.Add(datumToAdd);
+                            index--;
+                        }
+                    }
+                }
+            }
+            else if (granularity == Granularity.Month)
+            {
+                int monthsDifference = (toExcludedUtc.Month - fromIncludedUtc.Month);
+
+                if (monthsDifference == sortedDatumsList.Count() && sortedDatumsList[0].Timestamp == fromIncludedUtc)
+                {
+                    return sortedDatums;
+                }
+                else
+                {
+                    for (int index = 0, currentMonth = fromIncludedUtc.Month; currentMonth < toExcludedUtc.Month;
+                        index++, currentMonth++)
+                    {
+                        if (sortedDatumsList[index].Timestamp.Month != currentMonth)
+                        {
+                            Datum<T> datumToAdd = new Datum<T>()
+                            {
+                                Quality = Quality.None,
+                                Value = default(T),
+                                Timestamp = fromIncludedUtc,
+                            };
+
+                            datumToAdd.Timestamp = new DateTime(fromIncludedUtc.Year, currentMonth, fromIncludedUtc.Day, fromIncludedUtc.Hour, fromIncludedUtc.Minute, fromIncludedUtc.Second);
+
+                            newList.Add(datumToAdd);
+                            index--;
+                        }
+                    }
+                }
+            }
+            else if (granularity == Granularity.Week)
+            {
+                
+            }
+            else if (granularity == Granularity.Day)
+            {
+                int daysDifference = (toExcludedUtc.Day - fromIncludedUtc.Day);
+
+                if (daysDifference == sortedDatumsList.Count() && sortedDatumsList[0].Timestamp == fromIncludedUtc)
+                {
+                    return sortedDatums;
+                }
+                else
+                {
+                    for (int index = 0, currentDay = fromIncludedUtc.Day; currentDay < toExcludedUtc.Day;
+                        index++, currentDay++)
+                    {
+                        if (sortedDatumsList[index].Timestamp.Day != currentDay)
+                        {
+                            Datum<T> datumToAdd = new Datum<T>()
+                            {
+                                Quality = Quality.None,
+                                Value = default(T),
+                                Timestamp = fromIncludedUtc,
+                            };
+
+                            datumToAdd.Timestamp = new DateTime(fromIncludedUtc.Year, fromIncludedUtc.Month, currentDay, fromIncludedUtc.Hour, fromIncludedUtc.Minute, fromIncludedUtc.Second);
+
+                            newList.Add(datumToAdd);
+                            index--;
+                        }
+                    }
+                }
+            }
+            else if (granularity == Granularity.Hour)
+            {
+                int hoursDifference = (toExcludedUtc.Hour - fromIncludedUtc.Hour);
+
+                if (hoursDifference == sortedDatumsList.Count() && sortedDatumsList[0].Timestamp == fromIncludedUtc)
+                {
+                    return sortedDatums;
+                }
+                else
+                {
+                    for (int index = 0, currentHour = fromIncludedUtc.Hour; currentHour < toExcludedUtc.Hour;
+                        index++, currentHour++)
+                    {
+                        if (sortedDatumsList[index].Timestamp.Hour != currentHour)
+                        {
+                            Datum<T> datumToAdd = new Datum<T>()
+                            {
+                                Quality = Quality.None,
+                                Value = default(T),
+                                Timestamp = fromIncludedUtc,
+                            };
+
+                            datumToAdd.Timestamp = new DateTime(fromIncludedUtc.Year, fromIncludedUtc.Month, fromIncludedUtc.Day, currentHour, fromIncludedUtc.Minute, fromIncludedUtc.Second);
+
+                            newList.Add(datumToAdd);
+                            index--;
+                        }
+                    }
+                }
+            }
+            else if (granularity == Granularity.Minute)
+            {
+                int minutesDifference = (toExcludedUtc.Minute - fromIncludedUtc.Minute);
+
+                if (minutesDifference == sortedDatumsList.Count() && sortedDatumsList[0].Timestamp == fromIncludedUtc)
+                {
+                    return sortedDatums;
+                }
+                else
+                {
+                    for (int index = 0, currentMinute = fromIncludedUtc.Minute; currentMinute < toExcludedUtc.Minute;
+                        index++, currentMinute++)
+                    {
+                        if (sortedDatumsList[index].Timestamp.Minute != currentMinute)
+                        {
+                            Datum<T> datumToAdd = new Datum<T>()
+                            {
+                                Quality = Quality.None,
+                                Value = default(T),
+                                Timestamp = fromIncludedUtc,
+                            };
+
+                            datumToAdd.Timestamp = new DateTime(fromIncludedUtc.Year, fromIncludedUtc.Month, fromIncludedUtc.Day, fromIncludedUtc.Hour, currentMinute, fromIncludedUtc.Second);
+
+                            newList.Add(datumToAdd);
+                            index--;
+                        }
+                    }
+                }
+            }
+            else if (granularity == Granularity.Second)
+            {
+                int secondsDifference = (toExcludedUtc.Second - fromIncludedUtc.Second);
+
+                if (secondsDifference == sortedDatumsList.Count() && sortedDatumsList[0].Timestamp == fromIncludedUtc)
+                {
+                    return sortedDatums;
+                }
+                else
+                {
+                    for (int index = 0, currentSecond = fromIncludedUtc.Second; currentSecond < toExcludedUtc.Second;
+                        index++, currentSecond++)
+                    {
+                        if (sortedDatumsList[index].Timestamp.Second != currentSecond)
+                        {
+                            Datum<T> datumToAdd = new Datum<T>()
+                            {
+                                Quality = Quality.None,
+                                Value = default(T),
+                                Timestamp = fromIncludedUtc,
+                            };
+
+                            datumToAdd.Timestamp = new DateTime(fromIncludedUtc.Year, fromIncludedUtc.Month, fromIncludedUtc.Day, fromIncludedUtc.Hour, fromIncludedUtc.Minute, currentSecond);
+
+                            newList.Add(datumToAdd);
+                            index--;
+                        }
+                    }
+                }
+            }
+
+            sortedDatumsList.AddRange(newList);
+            return sortedDatumsList.OrderBy(datum => datum.Timestamp);
         }
     }
 }
