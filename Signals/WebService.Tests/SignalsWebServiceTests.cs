187e3c3fea75d20b12ad7eba3e9b3a80b7f574d9
? <?>
2016-08-09 13:16:23 +0200

    Test Different time granularity (Hour)

--- Signals/WebService.Tests/SignalsWebServiceTests.cs
+++ Signals/WebService.Tests/SignalsWebServiceTests.cs
@@ -508,6 +508,30 @@ namespace WebService.Tests
                 Assert.IsTrue(result.Any(d => d.Quality != Dto.Quality.None));
             }
 
+            [TestMethod]
+            public void GivenASignalAndDataAndMVPByHour_WhenGettingData_ReturnsMissingValuesAccordingToNoneQualityMVP()
+            {
+                int signalId = 1;
+                GivenASignal(SignalWith(
+                    signalId,
+                    DataType.Boolean,
+                    Granularity.Hour,
+                    Path.FromString("")));
+
+                GivenData(signalId, new[]
+                {
+                    new Datum<bool> {Quality = Quality.Fair, Timestamp = new System.DateTime() },
+                    new Datum<bool> {Quality = Quality.Good, Timestamp = new System.DateTime().AddHours(1) }
+                });
+
+                GivenMissingValuePolicy(signalId, new NoneQualityMissingValuePolicyBoolean());
+
+                var result = signalsWebService.GetData(signalId, new System.DateTime(), new System.DateTime().AddHours(2));
+
+                Assert.IsTrue(result.Count() == 2);
+                Assert.IsTrue(result.Any(d => d.Quality != Dto.Quality.None));
+            }
+
             private Dto.Signal SignalWith(Dto.DataType dataType, Dto.Granularity granularity, Dto.Path path)
             {
                 return new Dto.Signal()
