117c013452022910f3194e66e534363a327e9aec
? <?>
2016-08-11 12:01:49 +0200

    test GivenDataAndSignal_WhenSetData_VerifyIfAddedDatums_FormHourGranularity() passes

--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -161,6 +161,24 @@ namespace Domain.Services.Implementation
                 }
             }
 
+            if (dataDomainList.First().Signal.Granularity == Granularity.Hour)
+            {
+                for (int i = 0; i < dataDomainList.Count - 1; i++)
+                {
+                    if (dataDomainList[i].Timestamp.CompareTo(dataDomainList[i + 1].Timestamp.AddHours(-1)) != 0)
+                    {
+                        missingDatas.Add(new Datum<T>()
+                        {
+                            Id = 0,
+                            Quality = Quality.None,
+                            Timestamp = dataDomainList[i].Timestamp.AddHours(1),
+                            Signal = dataDomainList[i].Signal,
+                            Value = default(T)
+                        });
+                    }
+                }
+            }
+
             if (dataDomainList.First().Signal.Granularity == Granularity.Day)
             {
                 for (int i = 0; i < dataDomainList.Count - 1; i++)
