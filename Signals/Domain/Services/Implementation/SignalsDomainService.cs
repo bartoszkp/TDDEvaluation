b47a2ea7b76d3645d6b53dd31247e6963a9f35f1
? <?>
2016-08-11 11:58:01 +0200

    test GivenDataAndSignal_WhenSetData_VerifyIfAddedDatums_FormMinuteGranularity() passes

--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -143,6 +143,24 @@ namespace Domain.Services.Implementation
                 }
             }
 
+            if (dataDomainList.First().Signal.Granularity == Granularity.Minute)
+            {
+                for (int i = 0; i < dataDomainList.Count - 1; i++)
+                {
+                    if (dataDomainList[i].Timestamp.CompareTo(dataDomainList[i + 1].Timestamp.AddMinutes(-1)) != 0)
+                    {
+                        missingDatas.Add(new Datum<T>()
+                        {
+                            Id = 0,
+                            Quality = Quality.None,
+                            Timestamp = dataDomainList[i].Timestamp.AddMinutes(1),
+                            Signal = dataDomainList[i].Signal,
+                            Value = default(T)
+                        });
+                    }
+                }
+            }
+
             if (dataDomainList.First().Signal.Granularity == Granularity.Day)
             {
                 for (int i = 0; i < dataDomainList.Count - 1; i++)
