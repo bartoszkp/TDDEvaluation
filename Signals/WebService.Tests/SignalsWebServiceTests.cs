cf76c28a170cb05627b06cea888b1baa6fdeeb31
? <?>
2016-08-11 12:06:53 +0200

    test GivenDataAndSignal_WhenSetData_VerifyIfAddedDatums_FormWeekGranularity() fails

--- Signals/WebService.Tests/SignalsWebServiceTests.cs
+++ Signals/WebService.Tests/SignalsWebServiceTests.cs
@@ -481,23 +481,23 @@ namespace WebService.Tests
             }
 
             [TestMethod]
-            public void GivenDataAndSignal_WhenSetData_VerifyIfAddedDatums_FormHourGranularity()
+            public void GivenDataAndSignal_WhenSetData_VerifyIfAddedDatums_FormWeekGranularity()
             {
                 int signalId = 6;
 
                 Dto.Datum[] settedData = new Dto.Datum[]{
-                     new Dto.Datum() { Quality = Dto.Quality.Good, Timestamp = new DateTime(2000, 1, 1, 3, 1, 1), Value = (double) 1},
-                     new Dto.Datum() { Quality = Dto.Quality.Poor, Timestamp = new DateTime(2000, 1, 1, 5, 1, 1), Value = (double) 3}};
+                     new Dto.Datum() { Quality = Dto.Quality.Good, Timestamp = new DateTime(2000, 1, 1), Value = (double) 1},
+                     new Dto.Datum() { Quality = Dto.Quality.Poor, Timestamp = new DateTime(2000, 1, 15), Value = (double) 3}};
 
                 SetupWebService();
 
                 signalsRepositoryMock.Setup(srm => srm.Get(It.Is<int>(id => id == signalId)))
-                    .Returns(new Domain.Signal() { DataType = DataType.Double, Granularity = Granularity.Hour });
+                    .Returns(new Domain.Signal() { DataType = DataType.Double, Granularity = Granularity.Week });
 
                 signalsWebService.SetData(signalId, settedData);
 
                 signalsDataRepositoryMock.Verify(sdr => sdr.SetData<double>(It.Is<IEnumerable<Datum<double>>>(d =>
-                d.ElementAt(1).Timestamp == new DateTime(2000, 1, 1, 4 ,1 ,1)
+                d.ElementAt(1).Timestamp == new DateTime(2000, 1, 8)
                 && d.ElementAt(1).Quality == Quality.None
                 && d.ElementAt(1).Value == (double)0)));
             }
