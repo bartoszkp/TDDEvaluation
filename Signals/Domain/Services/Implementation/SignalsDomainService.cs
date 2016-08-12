f1e1bb2ed927f8ca1a16735891e74cb15d472437
? <?>
2016-08-12 14:24:39 +0200

    GivenASignalAndatumWithGranularityHour_WhenGettingData_ReturnListWithelementsNone() passsed

--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -134,6 +134,22 @@ namespace Domain.Services.Implementation
                         }
                     case Granularity.Hour:
                         {
+                            int countElementOfList = toExcludedUtc.Hour - fromIncludedUtc.Hour;
+                            if (countElementOfList + 1 == gettingList.Length)
+                                return gettingList;
+                            for (int i = 0; i < countElementOfList; i++)
+                            {
+
+                                Datum<T> xx = gettingList.FirstOrDefault(x => x.Timestamp == checkedDateTime);
+                                if (xx == null)
+                                {
+                                    var addingItem = new Datum<T>() { Quality = Quality.None, Timestamp = checkedDateTime, Value = default(T) };
+                                    returnList.Add(addingItem);
+                                }
+                                else
+                                    returnList.Add(xx);
+                                checkedDateTime = checkedDateTime.AddHours(1);
+                            }
                             break;
                         }
                     case Granularity.Day:
