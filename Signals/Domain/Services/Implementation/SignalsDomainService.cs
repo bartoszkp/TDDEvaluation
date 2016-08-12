fc4b8eabd80c6ad327e40ade9d0cf680dc0703f3
? <?>
2016-08-12 14:27:36 +0200

    GivenASignalAndatumWithGranularityMinute_WhenGettingData_ReturnListWithelementsNone() passsed

--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -110,6 +110,22 @@ namespace Domain.Services.Implementation
                 {
                     case Granularity.Minute:
                         {
+                            int countElementOfList = toExcludedUtc.Minute - fromIncludedUtc.Minute;
+                            if (countElementOfList + 1 == gettingList.Length)
+                                return gettingList;
+                            for (int i = 0; i < countElementOfList; i++)
+                            {
+
+                                Datum<T> xx = gettingList.FirstOrDefault(x => x.Timestamp == checkedDateTime);
+                                if (xx == null)
+                                {
+                                    var addingItem = new Datum<T>() { Quality = Quality.None, Timestamp = checkedDateTime, Value = default(T) };
+                                    returnList.Add(addingItem);
+                                }
+                                else
+                                    returnList.Add(xx);
+                                checkedDateTime = checkedDateTime.AddMinutes(1);
+                            }
                             break;
                         }
                     case Granularity.Month:
