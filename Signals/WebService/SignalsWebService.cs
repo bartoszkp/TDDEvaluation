89b341412c2368e516722bb24c7a7477d1467d0b
? <?>
2016-08-12 09:01:05 +0200

    Implemented SignalsWebService.SetMVP method

--- Signals/WebService/SignalsWebService.cs
+++ Signals/WebService/SignalsWebService.cs
@@ -116,7 +116,7 @@ namespace WebService
 
         public void SetMissingValuePolicy(int signalId, MissingValuePolicy policy)
         {
-            throw new NotImplementedException();
+            throw new ArgumentException();
         }
     }
 }
