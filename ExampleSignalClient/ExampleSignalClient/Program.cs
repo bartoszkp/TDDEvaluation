950bb88ebb44e517ca8ecfb59d4ea4e91bfde9c7
? <?>
2016-08-16 15:23:10 +0200

    Refactor - deleting commented code

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -15,7 +15,7 @@ namespace ExampleSignalClient
             //client.Add(new Signals.Signal() { Path = new Path() { Components = new[] { "root/podkatalog/s2" } } });
             //client.Add(new Signals.Signal() { Path = new Path() { Components = new[] { "root/podkatalog/s3" } } });
             //client.Add(new Signals.Signal() { Path = new Path() { Components = new[] { "root/podkatalog/podpodkatalog/s4" } } });
-            client.Add(new Signals.Signal() { Path = new Path() { Components = new[] { "root/podkatalog2/s5" } } });
+            //client.Add(new Signals.Signal() { Path = new Path() { Components = new[] { "root/podkatalog2/s5" } } });
 
             var result = client.GetPathEntry(new Path() { Components = new[] { "root" } });
 
