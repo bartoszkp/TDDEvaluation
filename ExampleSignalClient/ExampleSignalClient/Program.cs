5454023bca4096ebe9450c73caae0aabf1551596
? <?>
2016-08-16 12:44:41 +0200

    Update test program for #12

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -9,15 +9,17 @@ namespace ExampleSignalClient
         {
             SignalsWebServiceClient client = new SignalsWebServiceClient("BasicHttpBinding_ISignalsWebService");
 
-            client.SetMissingValuePolicy(1, new NoneQualityMissingValuePolicy() { DataType = DataType.Double });
+            int id = client.Add(new Signal() { DataType = DataType.Double }).Id.Value;
 
-            client.SetData(1, new Datum[]
+            client.SetMissingValuePolicy(id, new NoneQualityMissingValuePolicy() { DataType = DataType.Double });
+
+            client.SetData(id, new Datum[]
             {
                 new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 1, 1), Value = (double)1.5 },
                 new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 6, 1), Value = (double)2.5 }
             });
 
-            var result = client.GetData(1, new DateTime(2000, 1, 1), new DateTime(2000, 12, 1));
+            var result = client.GetData(id, new DateTime(2000, 1, 1), new DateTime(2000, 1, 1));
 
             foreach (var d in result)
             {
