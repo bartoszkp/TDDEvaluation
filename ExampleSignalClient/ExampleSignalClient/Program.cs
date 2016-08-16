9d4355feeb89f3414b4943396902e3b92373a31a
? <?>
2016-08-16 13:28:29 +0200

    WhenGettingSignalsWithGivenPathPrefix_ReturnsNull() passes, null returned

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -8,34 +8,26 @@ namespace ExampleSignalClient
         static void Main(string[] args)
         {
             SignalsWebServiceClient client = new SignalsWebServiceClient("BasicHttpBinding_ISignalsWebService");
-
-            var mvp1 = new Signals.SpecificValueMissingValuePolicy() { DataType = DataType.String, Quality = Quality.Fair, Value = "x" };
-            var mvp2 = new Signals.NoneQualityMissingValuePolicy();
-
-            client.Add(new Signals.Signal()
-            {
-                DataType = DataType.String,
-                Granularity = Granularity.Month,
-                Path = new Path() { Components = new[] { "sfd", "vbcx" } }
-            });
-            client.Add(new Signal()
+            
+            var signal = client.Add(new Signal()
             {
                 DataType = DataType.Double,
-                Granularity = Granularity.Month,
-                Path = new Path() { Components = new[] { "zxbcv", "zxvc" } }
+                Granularity = Granularity.Second,
+                Path = new Path() { Components = new[] { "zercrqyv", "zcr" } }
             });
 
-            client.SetMissingValuePolicy(1, mvp1);
-            client.SetMissingValuePolicy(2, mvp2);
+            client.SetData(signal.Id.Value, new Datum[] {
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 1, 1), Value = (double)1 },
+                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 2, 1), Value = (double)1.5 },
+                new Datum() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 3, 1), Value = (double)2 } });
 
-            var result1 = client.GetMissingValuePolicy(1) as Signals.SpecificValueMissingValuePolicy;
-            var result2 = client.GetMissingValuePolicy(2) as Signals.NoneQualityMissingValuePolicy;
+            var result = client.GetData(signal.Id.Value, new DateTime(2000, 1, 1), new DateTime(2000, 1, 1, 0, 0, 10));
 
-            Console.WriteLine(result1.Signal.Id.Value);
-            Console.WriteLine(result2.Signal.Id.Value);
-            Console.WriteLine(result1.DataType);
-            Console.WriteLine(result1.Quality);
-            Console.WriteLine(result1.Value);
+            foreach (var d in result)
+            {
+                Console.WriteLine(d.Timestamp.ToString() + ": " + d.Value.ToString() + " (" + d.Quality.ToString() + ")");
+            }
+
             Console.ReadKey();
 
         }
