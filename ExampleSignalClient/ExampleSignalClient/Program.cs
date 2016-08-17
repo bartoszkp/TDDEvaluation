112434e852ff0a0f14a59867174d8358ff20f292
? <?>
2016-08-17 12:40:06 +0200

    Example Client for SpecificValueMissingValuePolicy should correctly fill missing data

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -8,24 +8,27 @@ namespace ExampleSignalClient
         static void Main(string[] args)
         {
             SignalsWebServiceClient client = new SignalsWebServiceClient("BasicHttpBinding_ISignalsWebService");
-            
-            var signal = client.Add(new Signal()
+
+            var signal1 = client.Add(new Signal()
             {
                 DataType = DataType.Double,
-                Granularity = Granularity.Second,
-                Path = new Path() { Components = new[] { "zercrqyv", "zcr" } }
+                Granularity = Granularity.Month,
+                Path = new Path() { Components = new[] { "asdfgew" } }
             });
 
-            client.SetData(signal.Id.Value, new Datum[] {
-                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 1, 1), Value = (double)1 },
-                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 2, 1), Value = (double)1.5 },
-                new Datum() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 3, 1), Value = (double)2 } });
+            client.SetMissingValuePolicy(signal1.Id.Value, new SpecificValueMissingValuePolicy() { DataType = DataType.Double, Value = (double)42.42, Quality = Quality.Fair });
 
-            var result = client.GetData(signal.Id.Value, new DateTime(2000, 1, 1), new DateTime(2000, 1, 1, 0, 0, 10));
+            client.SetData(signal1.Id.Value, new Datum[]
+            {
+                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 1, 1), Value = (double)1.5 },
+                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 3, 1), Value = (double)2.5 }
+            });
+
+            var result = client.GetData(signal1.Id.Value, new DateTime(2000, 1, 1), new DateTime(2000, 4, 1));
 
             foreach (var d in result)
             {
-                Console.WriteLine(d.Timestamp.ToString() + ": " + d.Value.ToString() + " (" + d.Quality.ToString() + ")");
+                Console.WriteLine(d.Timestamp + ": " + d.Value + " (" + d.Quality + ")");
             }
 
             Console.ReadKey();
