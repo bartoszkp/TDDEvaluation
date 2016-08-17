5e9c8116ab15337481e69646b85088f12623b3c4
? <?>
2016-08-17 15:47:14 +0200

    NUT: Reading signal data with 'to' timestamp less than 'from' should return empty result

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -9,16 +9,17 @@ namespace ExampleSignalClient
         {
             SignalsWebServiceClient client = new SignalsWebServiceClient("BasicHttpBinding_ISignalsWebService");
 
-            var signal = client.Add(new Signal()
-            {
-                DataType = DataType.Decimal,
-                Granularity = Granularity.Second,
-                Path = new Path() { Components = new[] { "signal" } }
-            });
+            client.Add(new Signal() { DataType = DataType.Double, Granularity = Granularity.Month, Path = new Path() { Components = new[] { "root", "someSignal" } } });
 
-            var result = client.GetData(signal.Id.Value, new DateTime(2000, 1, 1), new DateTime(2000, 1, 1, 0, 1, 0));
+            client.SetData(1, new Datum[] {
+                         new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 1, 1), Value = (double)1 },
+                         new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 2, 1), Value = (double)1.5 },
+                         new Datum() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 3, 1), Value = (double)2 } });
+
+            var result = client.GetData(1, new DateTime(2000, 3, 1), new DateTime(2000, 1, 1));
 
             Console.WriteLine(result.Length);
+
             Console.ReadKey();
         }
     }
