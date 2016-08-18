6b4e206d38a6df7095093d124f55ffd14b24ddaf
? <?>
2016-08-18 11:00:50 +0200

    SignalsWebService.SetData code refactoring

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -15,16 +15,16 @@ namespace ExampleSignalClient
                 Granularity = Granularity.Month
             });
 
-            client.SetMissingValuePolicy(1, new SpecificValueMissingValuePolicy() { DataType = DataType.Double, Value = (double)42.42, Quality = Quality.Fair });
+            //client.SetMissingValuePolicy(1, new SpecificValueMissingValuePolicy() { DataType = DataType.Double, Value = (double)42.42, Quality = Quality.Fair });
 
             client.SetData(1, new Datum[]
             {
-                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 2, 1), Value = (double)1.5 },
-                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 4, 1), Value = (double)2.5 },
-                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 6, 1), Value = (double)1.5 },
+                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 3, 1), Value = (double)1.5 },
+                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 2, 1), Value = (double)2.5 },
+                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 1, 1), Value = (double)1.5 },
             });
 
-            var result = client.GetData(1, new DateTime(2000, 1, 1), new DateTime(2000, 10, 1));
+            var result = client.GetData(1, new DateTime(2000, 1, 1), new DateTime(2000, 4, 1));
 
             foreach (var d in result)
             {
--- Signals/WebService/SignalsWebService.cs
+++ Signals/WebService/SignalsWebService.cs
@@ -175,116 +175,35 @@ namespace WebService
             if (signal == null)
                 throw new Domain.Exceptions.SettingNotExistingSignalDataException();
 
-            if(signal.DataType == Dto.DataType.Boolean)
+            if (signal.DataType == Dto.DataType.Boolean)
             {
-                List<Domain.Datum<bool>> domainDatumList = new List<Domain.Datum<bool>>();
+                var sortedDomainData = data.OrderBy(d => d.Timestamp).ToArray();
 
-                foreach (var item in data)
-                {
-                    Domain.Datum<bool> domainDatum = new Domain.Datum<bool>();
-
-                    domainDatum.Quality = item.Quality.ToDomain<Domain.Quality>();
-                    domainDatum.Timestamp = item.Timestamp;
-                    domainDatum.Value = Convert.ToBoolean(item.Value);
-
-                    domainDatumList.Add(domainDatum);
-                }
-
-                IEnumerable<Domain.Datum<bool>> domainData = domainDatumList.AsEnumerable<Domain.Datum<bool>>();
-
-                var sortedDomainData = domainData.OrderBy(d => d.Timestamp).AsEnumerable<Domain.Datum<bool>>();
-
-                signalsDomainService.SetData(signalId, sortedDomainData);
+                signalsDomainService.SetData<bool>(signalId, sortedDomainData.ToDomain<IEnumerable<Domain.Datum<bool>>>().ToArray());
             }
-            else if(signal.DataType == Dto.DataType.Decimal)
+            else if (signal.DataType == Dto.DataType.Decimal)
             {
-                List<Domain.Datum<decimal>> domainDatumList = new List<Domain.Datum<decimal>>();
+                var sortedDomainData = data.OrderBy(d => d.Timestamp).ToArray();
 
-                foreach (var item in data)
-                {
-                    Domain.Datum<decimal> domainDatum = new Domain.Datum<decimal>();
-
-                    domainDatum.Quality = item.Quality.ToDomain<Domain.Quality>();
-                    domainDatum.Timestamp = item.Timestamp;
-                    domainDatum.Value = Convert.ToDecimal(item.Value);
-
-                    domainDatumList.Add(domainDatum);
-                }
-
-                IEnumerable<Domain.Datum<decimal>> domainData = domainDatumList.AsEnumerable<Domain.Datum<decimal>>();
-
-                var sortedDomainData = domainData.OrderBy(d => d.Timestamp).AsEnumerable<Domain.Datum<decimal>>();
-
-                signalsDomainService.SetData(signalId, sortedDomainData);
+                signalsDomainService.SetData<decimal>(signalId, sortedDomainData.ToDomain<IEnumerable<Domain.Datum<decimal>>>().ToArray());
             }
-            else if(signal.DataType == Dto.DataType.Double)
+            else if (signal.DataType == Dto.DataType.Double)
             {
-                List<Domain.Datum<double>> domainDatumList = new List<Domain.Datum<double>>();
+                var sortedDomainData = data.OrderBy(d => d.Timestamp).ToArray();
 
-                foreach (var item in data)
-                {
-                    Domain.Datum<double> domainDatum = new Domain.Datum<double>();
-
-                    domainDatum.Quality = item.Quality.ToDomain<Domain.Quality>();
-                    domainDatum.Timestamp = item.Timestamp;
-                    domainDatum.Value = Convert.ToDouble(item.Value);
-
-                    domainDatumList.Add(domainDatum);
-                }
-
-                IEnumerable<Domain.Datum<double>> domainData = domainDatumList.AsEnumerable<Domain.Datum<double>>();
-
-                var sortedDomainData = domainData.OrderBy(d => d.Timestamp).AsEnumerable<Domain.Datum<double>>();
-
-                signalsDomainService.SetData(signalId, sortedDomainData);
+                signalsDomainService.SetData(signalId, sortedDomainData.ToDomain<IEnumerable<Domain.Datum<double>>>().ToArray());
             }
             else if (signal.DataType == Dto.DataType.Integer)
             {
-                List<Domain.Datum<int>> domainDatumList = new List<Domain.Datum<int>>();
+                var sortedDomainData = data.OrderBy(d => d.Timestamp).ToArray();
 
-                foreach (var item in data)
-                {
-                    Domain.Datum<int> domainDatum = new Domain.Datum<int>();
-
-                    domainDatum.Quality = item.Quality.ToDomain<Domain.Quality>();
-                    domainDatum.Timestamp = item.Timestamp;
-                    domainDatum.Value = Convert.ToInt32(item.Value);
-
-                    domainDatumList.Add(domainDatum);
-                }
-
-                IEnumerable<Domain.Datum<int>> domainData = domainDatumList.AsEnumerable<Domain.Datum<int>>();
-
-                var sortedDomainData = domainData.OrderBy(d => d.Timestamp).AsEnumerable<Domain.Datum<int>>();
-
-                signalsDomainService.SetData(signalId, sortedDomainData);
+                signalsDomainService.SetData<int>(signalId, sortedDomainData.ToDomain<IEnumerable<Domain.Datum<int>>>().ToArray());
             }
             else if (signal.DataType == Dto.DataType.String)
             {
-                List<Domain.Datum<string>> domainDatumList = new List<Domain.Datum<string>>();
+                var sortedDomainData = data.OrderBy(d => d.Timestamp).ToArray();
 
-                foreach (var item in data)
-                {
-                    Domain.Datum<string> domainDatum = new Domain.Datum<string>();
-
-                    domainDatum.Quality = item.Quality.ToDomain<Domain.Quality>();
-                    domainDatum.Timestamp = item.Timestamp;
-                    if (item.Value == null)
-                    {
-                        domainDatum.Value = null;
-                    }
-                    else
-                    {
-                        domainDatum.Value = Convert.ToString(item.Value);
-                    }
-                    domainDatumList.Add(domainDatum);
-                }
-
-                IEnumerable<Domain.Datum<string>> domainData = domainDatumList.AsEnumerable<Domain.Datum<string>>();
-
-                var sortedDomainData = domainData.OrderBy(d => d.Timestamp).AsEnumerable<Domain.Datum<string>>();
-
-                signalsDomainService.SetData(signalId, sortedDomainData);
+                signalsDomainService.SetData<string>(signalId, sortedDomainData.ToDomain<IEnumerable<Domain.Datum<string>>>().ToArray());
             }
         }
 
