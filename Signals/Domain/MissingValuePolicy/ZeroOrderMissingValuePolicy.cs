79bbf627ddd17e7390bb3dcd0196c8fb2a94f193
? <?>
2016-08-22 10:49:44 +0200

    Implement ZeroOrderMVP

--- Signals/Domain/MissingValuePolicy/ZeroOrderMissingValuePolicy.cs
+++ Signals/Domain/MissingValuePolicy/ZeroOrderMissingValuePolicy.cs
@@ -1,10 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using Domain.Infrastructure;
 
 namespace Domain.MissingValuePolicy
 {
     public class ZeroOrderMissingValuePolicy<T> : MissingValuePolicy<T>
     {
+        public override IEnumerable<Datum<T>> SetMissingValue(Signal signal, IEnumerable<Datum<T>> datums, DateTime fromIncludedUtc, DateTime toExcludedUtc)
+        {
+            if (fromIncludedUtc > toExcludedUtc)
+                return new List<Datum<T>>();
+            else if (fromIncludedUtc == toExcludedUtc)
+            {
+                var datum = datums.FirstOrDefault(d => d.Timestamp == fromIncludedUtc);
+                return new[] { datum ?? Datum<T>.CreateNone(signal, fromIncludedUtc) };
+            }
+            else
+            {
+                List<Datum<T>> filledList = new List<Datum<T>>();
+
+                Granularity granularity = signal.Granularity;
+
+                DateTime tmp = fromIncludedUtc;
+
+                while (tmp < toExcludedUtc)
+                {
+                    AddToTheListSuitableDatum(filledList, tmp, datums, signal);
+
+                    tmp = DateHelper.NextDate(tmp, granularity);
+                }
+
+                return filledList;
+            }
+        }
+
+        private void AddToTheListSuitableDatum(List<Datum<T>> filledList, DateTime tmp, IEnumerable<Domain.Datum<T>> datums, Signal signal)
+        {
+            Datum<T> newDatum = datums.FirstOrDefault(datum => datum.Timestamp == tmp);
+
+            filledList.Add(newDatum ?? filledList.LastOrDefault() ?? Datum<T>.CreateNone(signal, tmp));
+        }
     }
 }
