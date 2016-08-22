f827c607b4a11c796292985ec4b8f48d3eaebcd1
? <?>
2016-08-22 10:35:19 +0200

    GivenASignalAndDatum_WhenGettingDataQueriesWithIncorrectWithGranularityHours_CallException() passed

--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -110,6 +110,14 @@ namespace Domain.Services.Implementation
                             }
                             break;
                         }
+                    case Granularity.Hour:
+                        {
+                            if ((item.Timestamp.Millisecond != 0) || (item.Timestamp.Second != 0) || (item.Timestamp.Minute != 0))
+                            {
+                                throw new TimestampHaveWrongFormatException();
+                            }
+                            break;
+                        }
                 }
             }
             if (fromIncludedUtc==toExcludedUtc)
