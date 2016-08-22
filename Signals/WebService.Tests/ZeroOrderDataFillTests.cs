7ff810d8fcf36938e68bee22071c27f6a7bc3cdf
? <?>
2016-08-22 10:33:30 +0200

    NoPreviousDatum_DefaultDatumInserted() fails

--- Signals/WebService.Tests/ZeroOrderDataFillTests.cs
+++ Signals/WebService.Tests/ZeroOrderDataFillTests.cs
@@ -20,8 +20,7 @@ namespace WebService.Tests
         [TestMethod]
         public void GivenASignal_GetData_FillsMissingData()
         {
-            SignalsDomainService domainService = new SignalsDomainService(signalsRepoMock.Object, dataRepoMock.Object, mvpRepoMock.Object);
-            signalsWebService = new SignalsWebService(domainService);
+            SetupWebService();
 
             int id = 1;
 
@@ -51,6 +50,44 @@ namespace WebService.Tests
             Assert.AreEqual(1.5, filledDatum.Value);
             
 
+        }
+
+        [TestMethod]
+        public void NoPreviousDatum_DefaultDatumInserted()
+        {
+            SetupWebService();
+            int id = 1;
+
+            var returnedSignal = new Signal() { Id = id, Granularity = Granularity.Month, DataType = DataType.Double };
+
+            Mock<ZeroOrderMissingValuePolicy<double>> zeroOrderMvpMock = new Mock<ZeroOrderMissingValuePolicy<double>>();
+
+            signalsRepoMock.Setup(sr => sr.Get(id)).Returns(returnedSignal);
+            mvpRepoMock.Setup(m => m.Get(returnedSignal))
+                .Returns(zeroOrderMvpMock.Object);
+
+            dataRepoMock.Setup(d => d.GetData<double>(returnedSignal, new DateTime(2000, 1, 1), new DateTime(2000, 4, 1)))
+                .Returns(new List<Datum<double>>()
+                {
+                    new Datum<double>() { Quality = Quality.Good, Timestamp = new DateTime(2000, 3, 1), Value = (double)2.5 }
+                });
+
+
+            var result = signalsWebService.GetData(id, new DateTime(2000, 2, 1), new DateTime(2000, 4, 1));
+
+            var filledDatum = result.ElementAt(0);
+
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual(Dto.Quality.None, filledDatum.Quality);
+            Assert.AreEqual(0, filledDatum.Value);
+
+
+        }
+
+        private void SetupWebService()
+        {
+            SignalsDomainService domainService = new SignalsDomainService(signalsRepoMock.Object, dataRepoMock.Object, mvpRepoMock.Object);
+            signalsWebService = new SignalsWebService(domainService);
         }
 
 
