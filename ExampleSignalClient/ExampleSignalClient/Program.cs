f4043239f62dce1f82b718e30514db3d58aac929
? <?>
2016-08-22 09:55:46 +0200

    Added client code

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -9,26 +9,19 @@ namespace ExampleSignalClient
         static void Main(string[] args)
         {
             SignalsWebServiceClient client = new SignalsWebServiceClient("BasicHttpBinding_ISignalsWebService");
-            
-            /*client.Add(new Signal() { Path = new Path() { Components = new[] { "s0" } }, Granularity = Granularity.Month, DataType = DataType.Double });
-            client.Add(new Signal() { Path = new Path() { Components = new[] { "root", "s1" } }, Granularity = Granularity.Month, DataType = DataType.Double });
-            client.Add(new Signal() { Path = new Path() { Components = new[] { "root", "s1", "s2" } }, Granularity = Granularity.Month, DataType = DataType.Double });
-            client.Add(new Signal() { Path = new Path() { Components = new[] { "root", "podkatalog", "s3" } }, Granularity = Granularity.Month, DataType = DataType.Double });
-            client.Add(new Signal() { Path = new Path() { Components = new[] { "root", "podkatalog", "podpodkatalog", "s4" } }, Granularity = Granularity.Month, DataType = DataType.Double });
-            client.Add(new Signal() { Path = new Path() { Components = new[] { "root", "podkatalog2", "s5" } }, Granularity = Granularity.Month, DataType = DataType.Double });
-            */
-            var result = client.GetPathEntry(new Path() { Components = new[] { "root", "s1" } });
 
-            Console.WriteLine("Sygnały w 'root':");
-            foreach (var r in result.Signals)
+            client.Add(new Signal()
             {
-                Console.WriteLine(string.Join("/", r.Path.Components) + ", " + r.Id);
-            }
-            Console.WriteLine("Ścieżki podrzędne w 'root':");
-            foreach (var s in result.SubPaths)
+                DataType = DataType.Integer,
+                Granularity = Granularity.Month
+            });
+
+            var data = new Datum[]
             {
-                Console.WriteLine(string.Join("/", s.Components));
-            }
+                 new Datum() { Quality = Quality.Bad, Value = 0, Timestamp = new DateTime(2000, 1, 1, 12, 45, 0) }
+            };
+
+            client.SetData(1, data);
 
             Console.ReadKey();
 
