dd9d8373b408d2ea955b44c0f372da0d44363baf
? <?>
2016-08-22 09:12:19 +0200

    Added GivenASignal_WhenSettingMonthlyDataThatIsNotMidnightFirst_ArgumentExceptionIsThrown unit test

--- Signals/WebService.Tests/SignalsWebServiceTests.cs
+++ Signals/WebService.Tests/SignalsWebServiceTests.cs
@@ -881,6 +881,18 @@ namespace WebService.Tests
                 signalsWebService.SetData(id, new Datum[] { new Datum() { Timestamp = new DateTime(2000, 3, 1) } });
             }
 
+            [ExpectedException(typeof(ArgumentException))]
+            [TestMethod]
+            public void GivenASignal_WhenSettingMonthlyDataThatIsNotMidnightFirst_ArgumentExceptionIsThrown()
+            {
+                var id = 1;
+                MakeMocks();
+                GivenASignal(SignalWith(id, Domain.DataType.Double, Domain.Granularity.Month, Domain.Path.FromString("a/b/c")));
+
+                signalsWebService.SetData(id, new Datum[] { new Datum() { Timestamp = new DateTime(2000, 1, 1, 0, 0, 1) } });
+            }
+
+
             #endregion
 
             private void SetupGetAllWithPathPrefix(IEnumerable<Domain.Signal> signals)
