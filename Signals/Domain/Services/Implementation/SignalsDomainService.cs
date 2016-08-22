16e87c1b8681a7d6f7c198943834027c420e0632
? <?>
2016-08-22 12:41:54 +0200

    Added: CheckDatasDateTimeCorrection and CheckTimestampsCorrection methods

--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -80,11 +80,16 @@ namespace Domain.Services.Implementation
 
         public void SetData<T>(IEnumerable<Datum<T>> domainData)
         {
+            CheckDatasDateTimeCorrection(domainData);
+
             signalsDataRepository.SetData<T>(domainData);
         }
+   
 
         public IEnumerable<Datum<T>> GetData<T>(Signal signal, DateTime fromIncludedUtc, DateTime toExcludedUtc)
         {
+            CheckTimestampsCorrection(signal, fromIncludedUtc);
+
             var getData = signalsDataRepository
                 .GetData<T>(signal, fromIncludedUtc, toExcludedUtc)
                 ?.OrderBy(s => s.Timestamp).ToArray();
@@ -112,17 +117,6 @@ namespace Domain.Services.Implementation
             return result;
         }
 
-        private Datum<T> GenerateDatumFromPolicy<T>(MissingValuePolicy<T> mvp, Signal signal, DateTime timestamp)
-        {
-            var result = new Datum<T>()
-            {
-                Signal = signal,
-                Timestamp = timestamp
-            };
-
-            return mvp.FillMissingValue(result);
-        }
-
         public PathEntry GetPathEntry(Path pathDomain)
         {
             var signals = signalsRepository.GetAllWithPathPrefix(pathDomain);
@@ -136,6 +130,39 @@ namespace Domain.Services.Implementation
             var result = new PathEntry(filteredSignals, subFolders);
 
             return result;
+        }
+
+        private void CheckDatasDateTimeCorrection<T>(IEnumerable<Datum<T>> data)
+        {
+            foreach (var d in data)
+            {
+                CheckTimestampsCorrection(d.Signal, d.Timestamp);
+            }
+        }
+
+        private void CheckTimestampsCorrection(Signal signal, DateTime dateTime)
+        {
+            if ((signal.Granularity == Granularity.Second && dateTime.Millisecond != 0)
+                || (signal.Granularity == Granularity.Minute && (dateTime.Second != 0 || dateTime.Millisecond != 0) )
+                || (signal.Granularity == Granularity.Hour && (dateTime.Minute != 0 || dateTime.Second != 0 || dateTime.Millisecond != 0) )
+                || (signal.Granularity == Granularity.Day && dateTime.TimeOfDay.Ticks != 0)
+                || (signal.Granularity == Granularity.Week && (dateTime.TimeOfDay.Ticks != 0 || dateTime.DayOfWeek != DayOfWeek.Monday) )
+                || (signal.Granularity == Granularity.Month
+                    && (dateTime.TimeOfDay.Ticks != 0 || DateTime.Compare(dateTime, new DateTime(dateTime.Year, dateTime.Month, 1)) != 0))
+                || (signal.Granularity == Granularity.Year 
+                    && (dateTime.TimeOfDay.Ticks != 0 || DateTime.Compare(dateTime, new DateTime(dateTime.Year, 1, 1)) != 0)) )
+                throw new ArgumentException("DateTime is incorrect");
+        }
+
+        private Datum<T> GenerateDatumFromPolicy<T>(MissingValuePolicy<T> mvp, Signal signal, DateTime timestamp)
+        {
+            var result = new Datum<T>()
+            {
+                Signal = signal,
+                Timestamp = timestamp
+            };
+
+            return mvp.FillMissingValue(result);
         }
     }
 }
