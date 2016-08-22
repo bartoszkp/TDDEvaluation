df1f5dc7b797511c5231d0bf5bdb4d4dce650f27
? <?>
2016-08-22 09:11:31 +0200

    GivenASignal_GetData_FillsMissingData() fails

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -9,15 +9,27 @@ namespace ExampleSignalClient
         {
             SignalsWebServiceClient client = new SignalsWebServiceClient("BasicHttpBinding_ISignalsWebService");
 
-            client.SetMissingValuePolicy(1, new SpecificValueMissingValuePolicy() { DataType = DataType.Double, Value = (double)42.42, Quality = Quality.Fair });
+            Random random = new Random();
 
-            client.SetData(1, new Datum[]
+            Signal signal = new Signal()
             {
-        new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 1, 1), Value = (double)1.5 },
-        new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 3, 1), Value = (double)2.5 }
+                Granularity = Granularity.Month,
+                DataType = DataType.Double,
+                Path = new Path() { Components = new[] { "x" + random.Next(100), "y" + random.Next(100) } }
+            };
+
+
+            var id = client.Add(signal).Id;
+
+            client.SetMissingValuePolicy(id.GetValueOrDefault(), new SpecificValueMissingValuePolicy() { DataType = DataType.Double, Value = (double)42.42, Quality = Quality.Fair });
+
+            client.SetData(id.GetValueOrDefault(), new Datum[]
+            {
+                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 1, 1), Value = (double)1.5 },
+                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 3, 1), Value = (double)2.5 }
             });
 
-            var result = client.GetData(1, new DateTime(2000, 1, 1), new DateTime(2000, 4, 1));
+            var result = client.GetData(id.GetValueOrDefault(), new DateTime(2000, 1, 1), new DateTime(2000, 4, 1));
 
             foreach (var d in result)
             {
---
+++ Signals/WebService.Tests/SpecificDataFillTests.cs
@@ -0,0 +1,68 @@
+﻿using Domain.Repositories;
+using Domain.Services.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Domain.MissingValuePolicy;
+
+namespace WebService.Tests
+{
+    [TestClass]
+    public class SpecificDataFillTests
+    {
+
+        private SignalsWebService signalsWebService;
+
+        [TestMethod]
+        public void GivenASignal_GetData_FillsMissingData()
+        {
+            SignalsDomainService domainService = new SignalsDomainService(signalsRepoMock.Object, dataRepoMock.Object, mvpRepoMock.Object);
+            signalsWebService = new SignalsWebService(domainService);
+
+            int id = 1;
+
+            var returnedSignal = new Signal() { Id = id, Granularity = Granularity.Month, DataType = DataType.Double };
+
+            Mock<SpecificValueMissingValuePolicy<double>> specificMvpMock = new Mock<SpecificValueMissingValuePolicy<double>>();
+            specificMvpMock.Object.Value = (double)42.42;
+            specificMvpMock.Object.Quality = Quality.Fair;
+
+            signalsRepoMock.Setup(sr => sr.Get(id)).Returns(returnedSignal);
+            mvpRepoMock.Setup(m => m.Get(returnedSignal))
+                .Returns(specificMvpMock.Object);
+
+            dataRepoMock.Setup(d => d.GetData<double>(returnedSignal, new DateTime(2000, 1, 1), new DateTime(2000, 4, 1)))
+                .Returns(new List<Datum<double>>()
+                {
+                    new Datum<double>() { Quality = Quality.Good, Timestamp = new DateTime(2000, 1, 1), Value = (double)1.5 },
+                    new Datum<double>() { Quality = Quality.Good, Timestamp = new DateTime(2000, 3, 1), Value = (double)2.5 }
+
+                });
+
+
+            var result = signalsWebService.GetData(id, new DateTime(2000, 1, 1), new DateTime(2000, 4, 1));
+
+            var filledDatum = result.ElementAt(1);
+
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual(Quality.Fair, filledDatum.Quality);
+            Assert.AreEqual(42.42, filledDatum.Value);
+
+
+        }
+
+
+        private Mock<ISignalsRepository> signalsRepoMock = new Mock<ISignalsRepository>();
+        private Mock<ISignalsDataRepository> dataRepoMock = new Mock<ISignalsDataRepository>();
+        private Mock<IMissingValuePolicyRepository> mvpRepoMock = new Mock<IMissingValuePolicyRepository>();
+
+
+
+
+    }
+}
