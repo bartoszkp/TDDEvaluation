9db413ce7bc8d468b14cc0eb3aec72607f784f64
? <?>
2016-08-22 15:47:20 +0200

    If SpecificValueMissingValuePolicy is set, it fills missing data

--- Signals/Domain/Datum.cs
+++ Signals/Domain/Datum.cs
@@ -15,6 +15,17 @@ namespace Domain
                 Quality = Quality.None
             };
         }
+        public static Datum<T> CreateSpecific<T>(Signal signal, DateTime timestamp, Quality quality, T Value)
+        {
+            return new Datum<T>()
+            {
+                Id = 0,
+                Signal = signal,
+                Timestamp = timestamp,
+                Value = Value,
+                Quality = quality,
+            };
+        }
 
         public virtual int Id { get; set; }
 
