b20777c5f804e64d008f0a5704047368c7d11c25
? <?>
2016-08-22 09:26:36 +0200

    Added GivenADailySignal_WhenSettingDataThatIsNotMidnight_ArgumentExceptionIsThrown unit test

--- Signals/WebService.Tests/SignalsWebServiceTests.cs
+++ Signals/WebService.Tests/SignalsWebServiceTests.cs
@@ -922,6 +922,18 @@ namespace WebService.Tests
 
                 signalsWebService.SetData(id, new Datum[] { new Datum() { Timestamp = new DateTime(2000, 1, 3) } });
             }
+
+            [ExpectedException(typeof(ArgumentException))]
+            [TestMethod]
+            public void GivenADailySignal_WhenSettingDataThatIsNotMidnight_ArgumentExceptionIsThrown()
+            {
+                var id = 1;
+                MakeMocks();
+                GivenASignal(SignalWith(id, Domain.DataType.Double, Domain.Granularity.Day, Domain.Path.FromString("a/b/c")));
+
+                signalsWebService.SetData(id, new Datum[] { new Datum() { Timestamp = new DateTime(2000, 5, 10, 0, 1, 0) } });
+            }
+
             #endregion
 
             private void SetupGetAllWithPathPrefix(IEnumerable<Domain.Signal> signals)
