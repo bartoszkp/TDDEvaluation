b22d1d15b14e13c54dc2ea67ff5fc41377880ba7
? <?>
2016-08-23 11:59:39 +0200

    Example client code

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -9,26 +9,30 @@ namespace ExampleSignalClient
         {
             SignalsWebServiceClient client = new SignalsWebServiceClient("BasicHttpBinding_ISignalsWebService");
 
-            var sig = client.Add(new Signal()
+            var signal = client.Add(new Signal()
             {
-                DataType = DataType.Double,
+                DataType = DataType.Integer,
                 Granularity = Granularity.Month,
-                Path = new Signals.Path { Components = new[] { "example", "path" } },
+                Path = new Path() { Components = new[] { "example", "path7" } },
             });
 
-            client.SetData(sig.Id.Value, new Datum[] {
-                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 1, 1), Value = (double)1 },
-                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 2, 1), Value = (double)1.5 },
-                new Datum() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 3, 1), Value = (double)2 } });
+            var data = new Datum[]
+            {
+                new Datum() { Quality = Quality.Bad, Value = 0, Timestamp = new DateTime(2000, 1, 1, 0, 0, 0) }
+            };
 
-            var result = client.GetData(sig.Id.Value, new DateTime(2000, 1, 1), new DateTime(2000, 1, 1));
+            client.SetData(signal.Id.Value, data);
+
+            var result = client.GetData(signal.Id.Value, new DateTime(2000, 1, 1, 0, 0, 0), new DateTime(2000, 3, 1, 0, 0, 1));
 
             foreach (var d in result)
             {
-                Console.WriteLine(d.Timestamp.ToString() + ": " + d.Value.ToString() + " (" + d.Quality.ToString() + ")");
+                Console.WriteLine(d.Timestamp + ": " + d.Value + " (" + d.Quality + ")");
             }
 
             Console.ReadKey();
+
+
         }
     }
 }
