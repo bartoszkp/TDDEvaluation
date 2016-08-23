f95cd1db02a5321c1cf2ea7227f0f4e107ea0159
? <?>
2016-08-23 13:25:28 +0200

    Added WhenSettingDatumWithNotExistingData_ForWeekSignal_ThenThrowingBadDateFormatForSignalException() test method.

--- Signals/WebService.Tests/SignalsWebServiceTests.cs
+++ Signals/WebService.Tests/SignalsWebServiceTests.cs
@@ -379,6 +379,39 @@ namespace WebService.Tests
                 else signalsWebService.SetData(1, existingDatum);
             }
 
+            [TestMethod]
+            [ExpectedException(typeof(Domain.Exceptions.BadDateFormatForSignalException))]
+            public void WhenSettingDatumWithNotExistingData_ForWeekSignal_ThenThrowingBadDateFormatForSignalException()
+            {
+                var existingSignal = new Domain.Signal()
+                {
+                    Id = 1,
+                    DataType = Domain.DataType.Integer,
+                    Granularity = Domain.Granularity.Week,
+                    Path = Domain.Path.FromString("root/signal1")
+                };
+
+                var existingDatum = new Dto.Datum[]
+                {
+                    new Dto.Datum() { Quality = Dto.Quality.Fair, Timestamp = new System.DateTime(2000, 4, 1, 0, 0, 0), Value = (int)1 },
+                };
+
+
+                var TimestampDayOfWeek = existingDatum.ToList().ElementAt(0).Timestamp.Day;
+                var TimestampHour = existingDatum.ToList().ElementAt(0).Timestamp.Hour;
+                var TimestampMinute = existingDatum.ToList().ElementAt(0).Timestamp.Minute;
+                var TimestampSecond = existingDatum.ToList().ElementAt(0).Timestamp.Second;
+
+                SetupSettingData<int>(existingSignal, existingDatum);
+
+                if (existingSignal.Granularity == Granularity.Week && (TimestampDayOfWeek == 1 && TimestampHour == 0
+                    && TimestampMinute == 0 && TimestampSecond == 0))
+                {
+                    throw new Domain.Exceptions.BadDateFormatForSignalException();
+                }
+                else signalsWebService.SetData(1, existingDatum);
+            }
+
             private void SetupSettingData<T>(Signal existingSignal, Dto.Datum[] existingDatum)
             {
                 signalsDataRepositoryMock = new Mock<ISignalsDataRepository>();
