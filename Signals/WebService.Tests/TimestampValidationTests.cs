0be8559b135bca8b8513def185d373fc73b05507
? <?>
2016-08-29 13:54:43 +0200

    WhenSettigDataForMinuteSignal_WithInvalidMilliSecondTimestamp_InvalidTimestampExceptionIsThrown() fails

--- Signals/WebService.Tests/TimestampValidationTests.cs
+++ Signals/WebService.Tests/TimestampValidationTests.cs
@@ -161,6 +161,30 @@ namespace WebService.Tests
 
         [TestMethod]
         [ExpectedException(typeof(InvalidTimestampException))]
+        public void WhenSettigDataForMinuteSignal_WithInvalidSecondTimestamp_InvalidTimestampExceptionIsThrown()
+        {
+            SetupSignalWithSpecificGranularity(Domain.Granularity.Minute);
+
+            List<Dto.Datum> data = new List<Dto.Datum>()
+            { new Dto.Datum() {Quality = Dto.Quality.Bad,Timestamp = new DateTime(2000, 1, 1, 0, 0, 1, 0), Value = 0  } };
+
+            signalsWebService.SetData(1, data);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidTimestampException))]
+        public void WhenSettigDataForMinuteSignal_WithInvalidMilliSecondTimestamp_InvalidTimestampExceptionIsThrown()
+        {
+            SetupSignalWithSpecificGranularity(Domain.Granularity.Minute);
+
+            List<Dto.Datum> data = new List<Dto.Datum>()
+            { new Dto.Datum() {Quality = Dto.Quality.Bad,Timestamp = new DateTime(2000, 1, 1, 0, 0, 0, 1), Value = 0  } };
+
+            signalsWebService.SetData(1, data);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidTimestampException))]
         public void GetData_DayGranularity_InvalidTimestamp_ExceptionThrown()
         {
             SetupWebService();
