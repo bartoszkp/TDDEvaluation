74f551d7a635441f38493c00316e969164344f02
? <?>
2016-08-29 08:55:22 +0200

    when from timestamp is greater than to -> empty list returned

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -9,31 +9,24 @@ namespace ExampleSignalClient
         {
             SignalsWebServiceClient client = new SignalsWebServiceClient("BasicHttpBinding_ISignalsWebService");
 
-            var signal = client.Add(new Signal()
+            Random random = new Random();
+
+            var id = client.Add(new Signal()
             {
                 DataType = DataType.Double,
                 Granularity = Granularity.Month,
-                Path = new Path() { Components = new[] { "example", "path6" } },
-            });
+                Path = new Path() { Components = new[] { "signal" + random.Next(100)} }
+            }).Id.Value;
 
-            client.SetMissingValuePolicy(signal.Id.Value, new ZeroOrderMissingValuePolicy() { DataType = DataType.Double });
+            client.SetData(id, new Datum[] {
+              new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 1, 1), Value = (double)1 },
+              new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 2, 1), Value = (double)1.5 },
+              new Datum() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 3, 1), Value = (double)2 } });
 
-            client.SetData(signal.Id.Value, new Datum[]
-            {
-                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 3, 1), Value = (double)1.5 },
-                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 4, 1), Value = (double)2.5 },
-                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 6, 1), Value = (double)3.5 },
-                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 7, 1), Value = (double)4.5 },
-                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 9, 1), Value = (double)5.5 }
-            });
+            var result = client.GetData(id, new DateTime(2000, 3, 1), new DateTime(2000, 1, 1));
 
-            var result = client.GetData(signal.Id.Value, new DateTime(2000, 1, 1), new DateTime(2000, 10, 1));
-
-            foreach (var d in result)
-            {
-                Console.WriteLine(d.Timestamp + ": " + d.Value + " (" + d.Quality + ")");
-            }
-
+            Console.WriteLine(result.Length);
+            
             Console.ReadKey();
         }
     }
--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -122,6 +122,9 @@ namespace Domain.Services.Implementation
 
         public IEnumerable<Datum<T>> GetData<T>(Signal signal, DateTime fromIncludedUTC, DateTime toExcludedUTC)
         {
+            if (fromIncludedUTC > toExcludedUTC)
+                return new List<Datum<T>>();
+
             List<Datum<T>> dataToCheck = new List<Datum<T>>();
             dataToCheck.Add(new Datum<T>() { Timestamp = fromIncludedUTC });
             dataToCheck.Add(new Datum<T>() { Timestamp = toExcludedUTC });
