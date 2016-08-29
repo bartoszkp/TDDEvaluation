4f85c31310d2bac22045a8243504e7add0f75d1a
? <?>
2016-08-29 14:53:51 +0200

    created test for deleting signal

---
+++ Signals/WebService.Tests/SignalDeletionTests.cs
@@ -0,0 +1,39 @@
+﻿using Domain;
+using Domain.Exceptions;
+using Domain.Repositories;
+using Domain.Services.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebService.Tests
+{
+    [TestClass]
+    public class SignalDeletionTests
+    {
+        private SignalsWebService signalsWebService;
+
+        [TestMethod]
+        [ExpectedException(typeof(NoSuchSignalException))]
+        public void SignalNotExists_DeleteSignal_ExceptionThrown()
+        {
+            SignalsDomainService domainService = new SignalsDomainService(
+                    signalsRepositoryMock.Object, null, missingValuePolicyRepositoryMock.Object);
+            signalsWebService = new SignalsWebService(domainService);
+
+            signalsRepositoryMock.Setup(sr => sr.Get(1)).Returns((Signal)null);
+
+            signalsWebService.Delete(1);
+
+        }
+
+
+        private Mock<ISignalsRepository> signalsRepositoryMock = new Mock<ISignalsRepository>();
+        private Mock<IMissingValuePolicyRepository> missingValuePolicyRepositoryMock = new Mock<IMissingValuePolicyRepository>();
+
+    }
+}
