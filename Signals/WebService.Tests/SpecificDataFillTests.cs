afc7bbff42585cf9c3022543283818b6630330b7
? <?>
2016-08-29 14:39:01 +0200

    GivenASignal_WhenGettingData_SpecificGoodQualityPolicy_CorrectlyFillsMissingSecondData() fails, Specific Policy incorrect fills datum's quaity

--- Signals/WebService.Tests/SpecificDataFillTests.cs
+++ Signals/WebService.Tests/SpecificDataFillTests.cs
@@ -55,6 +55,55 @@ namespace WebService.Tests
 
         }
 
+        [TestMethod]
+        public void GivenASignal_WhenGettingData_SpecificGoodQualityPolicy_CorrectlyFillsMissingSecondData()
+        {
+            SignalsDomainService domainService = new SignalsDomainService(signalsRepoMock.Object, dataRepoMock.Object, mvpRepoMock.Object);
+            signalsWebService = new SignalsWebService(domainService);
+
+            int id = 1;
+
+            var returnedSignal = new Signal() { Id = id, Granularity = Granularity.Second, DataType = DataType.Double };
+
+            Mock<SpecificValueMissingValuePolicy<double>> specificMvpMock = new Mock<SpecificValueMissingValuePolicy<double>>();
+            specificMvpMock.Object.Value = 42.42;
+            specificMvpMock.Object.Quality = Quality.Good;
+
+            signalsRepoMock.Setup(sr => sr.Get(id)).Returns(returnedSignal);
+            mvpRepoMock.Setup(m => m.Get(returnedSignal))
+                .Returns(specificMvpMock.Object);
+
+            dataRepoMock.Setup(d => d.GetData<double>(returnedSignal, new DateTime(2000, 1, 1, 0, 0, 0), new DateTime(2000, 1, 1, 0, 0, 5)))
+                .Returns(new List<Datum<double>>()
+                {
+                    new Datum<double>() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 1, 1, 0, 0, 0), Value = (double)1.5 },
+                    new Datum<double>() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 1, 1, 0, 0, 1), Value = (double)2.5 }
+                });
+
+
+            var result = signalsWebService.GetData(id, new DateTime(2000, 1, 1, 0, 0, 0), new DateTime(2000, 1, 1, 0, 0, 5));
+            var expectedDatum = new List<Dto.Datum>()
+            {
+                new Dto.Datum() { Quality = Dto.Quality.Fair, Timestamp = new DateTime(2000, 1, 1, 0, 0, 0), Value = (double)1.5 },
+                new Dto.Datum() { Quality = Dto.Quality.Fair, Timestamp = new DateTime(2000, 1, 1, 0, 0, 1), Value = (double)2.5 },
+                new Dto.Datum() { Quality = Dto.Quality.Good, Timestamp = new DateTime(2000, 1, 1, 0, 0, 2), Value = (double)42.42 },
+                new Dto.Datum() { Quality = Dto.Quality.Good, Timestamp = new DateTime(2000, 1, 1, 0, 0, 3), Value = (double)42.42 },
+                new Dto.Datum() { Quality = Dto.Quality.Good, Timestamp = new DateTime(2000, 1, 1, 0, 0, 4), Value = (double)42.42 },
+            };
+
+            int i = 0;
+
+            Assert.AreEqual(5, result.Count());
+            foreach (var actualData in result)
+            {
+                Assert.AreEqual(expectedDatum[i].Quality, actualData.Quality);
+                Assert.AreEqual(expectedDatum[i].Timestamp, actualData.Timestamp);
+                Assert.AreEqual(expectedDatum[i].Value, actualData.Value);
+
+                i++;
+            }
+        }
+
 
         private Mock<ISignalsRepository> signalsRepoMock = new Mock<ISignalsRepository>();
         private Mock<ISignalsDataRepository> dataRepoMock = new Mock<ISignalsDataRepository>();
