e370d8f8e33d57842b8222f5376466b2f7dd4b91
? <?>
2016-08-30 12:47:51 +0200

    GivenAnHourSignal_WhenGettingDataFromMoreThanOneStepOlder_WithZeroPolicy_ItCorrectlyFillsMissingData() passes

--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -253,7 +253,8 @@ namespace Domain.Services.Implementation
                                     {
                                         if (i == 0)
                                         {
-                                            addingItem = new Datum<T>() { Quality = Quality.None, Timestamp = checkedDateTime, Value = default(T) };
+                                            returnList = signalsDataRepository.GetDataOlderThan<T>(signal, checkedDateTime, 1).ToList();
+                                            addingItem = null;
                                         }
                                         else
                                         {
@@ -266,7 +267,8 @@ namespace Domain.Services.Implementation
                                     {
                                         addingItem = new Datum<T>() { Quality = Quality.None, Timestamp = checkedDateTime, Value = default(T) };
                                     }
-                                    returnList.Add(addingItem);
+                                    if (addingItem != null)
+                                        returnList.Add(addingItem);
                                 }
                                 else
                                     returnList.Add(xx);
