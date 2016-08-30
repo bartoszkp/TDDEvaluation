6eb0148060be38f396281935d9a077504fcf965a
? <?>
2016-08-30 12:40:22 +0200

    GivenAMinuteSignal_WhenGettingDataFromMoreThanOneStepOlder_WithZeroPolicy_ItCorrectlyFillsMissingData() passes

--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -207,7 +207,8 @@ namespace Domain.Services.Implementation
                                     {
                                         if (i == 0)
                                         {
-                                            addingItem = new Datum<T>() { Quality = Quality.None, Timestamp = checkedDateTime, Value = default(T) };
+                                            returnList = signalsDataRepository.GetDataOlderThan<T>(signal, checkedDateTime, 1).ToList();
+                                            addingItem = null;
                                         } 
                                         else
                                         {
@@ -220,7 +221,8 @@ namespace Domain.Services.Implementation
                                     {
                                         addingItem = new Datum<T>() { Quality = Quality.None, Timestamp = checkedDateTime, Value = default(T) };
                                     }
-                                    returnList.Add(addingItem);
+                                    if (addingItem != null)
+                                        returnList.Add(addingItem);
                                 }
                                 else
                                     returnList.Add(xx);
