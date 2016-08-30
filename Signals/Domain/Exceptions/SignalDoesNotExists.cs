080d9331cfd8e84fb00784971bec1fe4a0b25d59
? <?>
2016-08-30 14:40:47 +0200

    SignalDoesNotExists exception added

---
+++ Signals/Domain/Exceptions/SignalDoesNotExists.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.Exceptions
+{
+    public class SignalDoesNotExists : Exception
+    {
+        public SignalDoesNotExists()
+            : base("You cannot delete a signal that does not exists")
+        {
+        }
+    }
+}
