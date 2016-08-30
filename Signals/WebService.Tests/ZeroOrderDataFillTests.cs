744281a49846b0fd0919e8c771cf6d3b35985892
? <?>
2016-08-30 13:53:27 +0200

    NoPreviousDatumForYearSignal_DefaultDatumIsInserted() fails

--- Signals/WebService.Tests/ZeroOrderDataFillTests.cs
+++ Signals/WebService.Tests/ZeroOrderDataFillTests.cs
@@ -173,6 +173,24 @@ namespace WebService.Tests
         }
 
         [TestMethod]
+        public void NoPreviousDatumForYearSignal_DefaultDatumIsInserted()
+        {
+            SetupMockRepositories(Granularity.Year, 1, new DateTime(2000, 1, 1, 0, 0, 0), new DateTime(2004, 1, 1, 0, 0, 0),
+                new List<Datum<double>>(), new DateTime(2000, 1, 1, 0, 0, 0));
+
+            var result = signalsWebService.GetData(1, new DateTime(2000, 1, 1, 0, 0, 0), new DateTime(2004, 1, 1, 0, 0, 0));
+            var expectedDatum = new List<Dto.Datum>()
+            {
+                new Dto.Datum() { Quality = Dto.Quality.None, Timestamp = new DateTime(2000, 1, 1, 0, 0, 0), Value = default(double) },
+                new Dto.Datum() { Quality = Dto.Quality.None, Timestamp = new DateTime(2001, 1, 1, 0, 0, 0), Value = default(double) },
+                new Dto.Datum() { Quality = Dto.Quality.None, Timestamp = new DateTime(2002, 1, 1, 0, 0, 0), Value = default(double) },
+                new Dto.Datum() { Quality = Dto.Quality.None, Timestamp = new DateTime(2003, 1, 1, 0, 0, 0), Value = default(double) },
+            };
+
+            AssertEqual(expectedDatum, result);
+        }
+
+        [TestMethod]
         public void GivenASecondSignal_WhenGettingDataFromMoreThanOneStepOlder_WithZeroPolicy_ItCorrectlyFillsMissingData()
         {
             SetupMockRepositories(Granularity.Second, 1, new DateTime(2000, 1, 1, 0, 0, 1), new DateTime(2000, 1, 1, 0, 0, 5),
