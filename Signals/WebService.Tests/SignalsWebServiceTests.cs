82e2cca3e3852e2c348ed185eff298c3a4128ed6
? <?>
2016-08-30 11:06:03 +0200

    GivenData_WhenGettingData_ReturnsFilledDataByFirstOrderMVPWithFloatPrecision

--- Signals/WebService.Tests/SignalsWebServiceTests.cs
+++ Signals/WebService.Tests/SignalsWebServiceTests.cs
@@ -1156,6 +1156,41 @@ namespace WebService.Tests
                 Assert_Datums(expectedData, returnedData.ToArray());
             }
 
+            [TestMethod]
+            public void GivenData_WhenGettingData_ReturnsFilledDataByFirstOrderMVPWithFloatPrecision()
+            {
+                SetupWebService();
+
+                var signal = SignalWith(Dto.DataType.Double, Dto.Granularity.Day, new Dto.Path { Components = new[] { "t", "w" } });
+                signal.Id = 5;
+
+                Dto.Datum[] data = new Datum[]
+                {
+                    new Datum() { Timestamp = new DateTime(1999, 12, 31), Value = 1d, Quality = Dto.Quality.Good },
+                    new Datum() { Timestamp = new DateTime(2000, 1, 4), Value = 7d, Quality = Dto.Quality.Fair }
+                };
+
+                SetupMocks_ForCheckingDatums<double>(signal, data);
+
+                missingValuePolicyRepositoryMock
+                    .Setup(x => x.Get(It.Is<Domain.Signal>(s => s.DataType == Domain.DataType.Double)))
+                    .Returns(new FirstOrderMissingValuePolicyDouble());
+
+                var returnedData = signalsWebService.GetData(signal.Id.Value, new DateTime(1999, 12, 31), new DateTime(2000, 1, 5));
+
+                Dto.Datum[] expectedData = new Datum[]
+               { 
+                    new Datum() { Timestamp = new DateTime(1999, 12, 31), Value = 1d, Quality = Dto.Quality.Good },
+                    new Datum() { Timestamp = new DateTime(2000, 1, 1), Value = 2.5d, Quality = Dto.Quality.Fair },
+                    new Datum() { Timestamp = new DateTime(2000, 1, 2), Value = 4d, Quality = Dto.Quality.Fair },
+                    new Datum() { Timestamp = new DateTime(2000, 1, 3), Value = 5.5d, Quality = Dto.Quality.Fair },
+                    new Datum() { Timestamp = new DateTime(2000, 1, 4), Value = 7d, Quality = Dto.Quality.Fair },
+                };
+
+                Assert.AreEqual(expectedData.Count(), returnedData.Count());
+                Assert_Datums(expectedData, returnedData.ToArray());
+            }
+
             #endregion
 
             private void SetupMocks_ForCheckingDatums<T>(Dto.Signal signal, IEnumerable<Dto.Datum> data)
