69dbf1b3eb04f821b1eb1dcbe2a115512fc267a0
? <?>
2016-09-01 13:28:41 +0200

    GivenAnIntegerMinuteSignal_WhenGettingDataWithTimeStampLeftAndRightOutOfRange_FirstOrderPolicy_CorrectlyFillsMissingData() passes

--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -295,7 +295,7 @@ namespace Domain.Services.Implementation
                                         var x1 = signalsDataRepository.GetDataNewerThan<T>(signal, checkedDateTime, 1);
 
 
-                                        if (x0.ElementAt(0).Quality == Quality.None || x1.ElementAt(0).Quality == Quality.None)
+                                        if (x0.Count() == 0 || x1.Count() == 0)
                                         {
                                             returnList.Add(new Datum<T>()
                                             {
@@ -329,6 +329,7 @@ namespace Domain.Services.Implementation
                                                 returnList.Add(itemToAdd);
                                                 checkedDateTime = checkedDateTime.AddMinutes(1);
                                             }
+                                            i--;
 
                                             checkedDateTime = checkedDateTime.AddMinutes(-1);
                                         }
