25235427546795a01b34fdf94c7b2d821fb3e4f7
? <?>
2016-09-01 14:04:41 +0200

    SignalExists_GetData_WithSameTimestaps_SingleDatumReturned failed

--- Signals/WebService.Tests/GetDataTest.cs
+++ Signals/WebService.Tests/GetDataTest.cs
@@ -76,7 +76,7 @@ namespace WebService.Tests
         }
 
         [TestMethod]
-        public void SignalExists_GetData_WithSameTimestaps_SingleDatumReturned()
+        public void SignalExists_GetData_WithSameTimestaps_SingleDatumReturned_Fill()
         {
             SetupWebService();
             var signal = new Signal()
@@ -102,6 +102,42 @@ namespace WebService.Tests
             Assert.AreEqual(0, fetchedDatumObject.Value);
             Assert.AreEqual(timestamp, fetchedDatumObject.Timestamp);
         }
+
+        [TestMethod]
+        public void SignalExists_GetData_WithSameTimestaps_SingleDatumReturned()
+        {
+            SetupWebService();
+            var signal = new Signal()
+            {
+                Id = 1,
+                DataType = Domain.DataType.Integer,
+                Granularity = Domain.Granularity.Month
+            };
+
+            List<Datum<int>> dbDatum = new List<Datum<int>>()
+            {
+                new Datum<int>() { Id = 1, Quality = Quality.Fair, Timestamp = new DateTime(2000, 1, 10), Value = 1 }
+            };
+
+
+            var timestamp = new DateTime(2000, 1, 1);
+            signalsRepoMock.Setup(sr => sr.Get(1)).Returns(signal);
+            Mock<NoneQualityMissingValuePolicy<int>> mvp = new Mock<NoneQualityMissingValuePolicy<int>>();
+            mvpRepoMock.Setup(m => m.Get(signal)).Returns(mvp.Object);
+
+            signalsDataRepoMock.Setup(s => s.GetData<int>(signal, timestamp, timestamp))
+                .Returns(dbDatum);
+
+            var result = signalsWebService.GetData(1, timestamp, timestamp);
+
+            var fetchedDatumObject = result.ElementAt(0);
+
+            Assert.AreEqual(1, result.Count());
+            Assert.AreEqual(Dto.Quality.None, fetchedDatumObject.Quality);
+            Assert.AreEqual(0, fetchedDatumObject.Value);
+            Assert.AreEqual(timestamp, fetchedDatumObject.Timestamp);
+        }
+
 
 
 
