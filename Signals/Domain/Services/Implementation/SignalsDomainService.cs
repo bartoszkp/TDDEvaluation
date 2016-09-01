09564cbf924621c46a009eb44e51274b17e534c1
? <?>
2016-09-01 11:20:10 +0200

    GivenAnIntegerMinuteSignal_WhenGettingDataWithTimeStampLeftAndRightOutOfRange_FirstOrderPolicy_CorrectlyFillsMissingData() passes

--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -304,29 +304,56 @@ namespace Domain.Services.Implementation
                                         var x0 = signalsDataRepository.GetDataOlderThan<T>(signal, checkedDateTime, 1);
                                         var x1 = signalsDataRepository.GetDataNewerThan<T>(signal, checkedDateTime, 1);
 
-                                        var timeDifference = (x1.ElementAt(0).Timestamp - x0.ElementAt(0).Timestamp).Minutes;
 
-                                        var qualityToAdd = x1.ElementAt(0).Quality;
-
-                                        decimal avarage = (Convert.ToDecimal((Convert.ChangeType(x1.ElementAt(0).Value, typeof(T)))) - Convert.ToDecimal(Convert.ChangeType(x0.ElementAt(0).Value, typeof(T)))) / timeDifference;
-                                        decimal valueToAdd = Convert.ToDecimal(Convert.ChangeType(x0.ElementAt(0).Value, typeof(T)));
-
-                                        for (int j = 0; j < timeDifference; j++, i++)
+                                        if (x0.ElementAt(0).Quality == Quality.None)
                                         {
-                                            valueToAdd += avarage;
-                                            var itemToAdd = new Datum<T>()
+                                            returnList.Add(new Datum<T>()
                                             {
-                                                Quality = qualityToAdd,
+                                                Quality = Quality.None,
                                                 Signal = signal,
                                                 Timestamp = checkedDateTime,
-                                                Value = (T)Convert.ChangeType(valueToAdd, typeof(T)),
-                                            };
-
-                                            returnList.Add(itemToAdd);
-                                            checkedDateTime = checkedDateTime.AddMinutes(1);
+                                                Value = default(T),
+                                            });
                                         }
 
-                                        checkedDateTime = checkedDateTime.AddMinutes(-1);
+                                        else if (x1.ElementAt(0).Quality == Quality.None)
+                                        {
+                                            returnList.Add(new Datum<T>()
+                                            {
+                                                Quality = Quality.None,
+                                                Signal = signal,
+                                                Timestamp = checkedDateTime,
+                                                Value = default(T),
+                                            });
+                                        }
+
+                                        else
+                                        {
+                                            var timeDifference = (x1.ElementAt(0).Timestamp - x0.ElementAt(0).Timestamp).Minutes;
+
+                                            var qualityToAdd = x1.ElementAt(0).Quality;
+
+                                            decimal avarage = (Convert.ToDecimal((Convert.ChangeType(x1.ElementAt(0).Value, typeof(T)))) - Convert.ToDecimal(Convert.ChangeType(x0.ElementAt(0).Value, typeof(T)))) / timeDifference;
+                                            decimal valueToAdd = Convert.ToDecimal(Convert.ChangeType(x0.ElementAt(0).Value, typeof(T)));
+
+                                            for (int j = 0; j < timeDifference; j++, i++)
+                                            {
+                                                valueToAdd += avarage;
+                                                var itemToAdd = new Datum<T>()
+                                                {
+                                                    Quality = qualityToAdd,
+                                                    Signal = signal,
+                                                    Timestamp = checkedDateTime,
+                                                    Value = (T)Convert.ChangeType(valueToAdd, typeof(T)),
+                                                };
+
+                                                returnList.Add(itemToAdd);
+                                                checkedDateTime = checkedDateTime.AddMinutes(1);
+                                            }
+
+                                            checkedDateTime = checkedDateTime.AddMinutes(-1);
+                                        }
+                                        
                                         addingItem = null;
                                     }
 
