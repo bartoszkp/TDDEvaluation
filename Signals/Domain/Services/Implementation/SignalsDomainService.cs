17cfb9cf844483a715b98f79d82e09ea6e274cbf
? <?>
2016-09-01 13:29:57 +0200

    GivenAnIntegerHourSignal_WhenGettingDataWithTimeStampLeftAndRightOutOfRange_FirstOrderPolicy_CorrectlyFillsMissingData() passes

--- Signals/Domain/Services/Implementation/SignalsDomainService.cs
+++ Signals/Domain/Services/Implementation/SignalsDomainService.cs
@@ -401,7 +401,7 @@ namespace Domain.Services.Implementation
                                         var x0 = signalsDataRepository.GetDataOlderThan<T>(signal, checkedDateTime, 1);
                                         var x1 = signalsDataRepository.GetDataNewerThan<T>(signal, checkedDateTime, 1);
 
-                                        if (x0.ElementAt(0).Quality == Quality.None || x1.ElementAt(0).Quality == Quality.None)
+                                        if (x0.Count() == 0|| x1.Count() == 0)
                                         {
                                             returnList.Add(new Datum<T>()
                                             {
@@ -435,6 +435,7 @@ namespace Domain.Services.Implementation
                                                 returnList.Add(itemToAdd);
                                                 checkedDateTime = checkedDateTime.AddHours(1);
                                             }
+                                            i--;
 
                                             checkedDateTime = checkedDateTime.AddHours(-1);
                                         }
