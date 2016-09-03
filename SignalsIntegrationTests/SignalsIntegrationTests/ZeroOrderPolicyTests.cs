e9d52732bb4e606d31c5ec8ea56b181defa5d8cd
bartoszkp <bartoszkp@gmail.com>
2016-09-03 22:45:22 +0200

    Added ZeroOrder tests for oter data types

--- SignalsIntegrationTests/SignalsIntegrationTests/ZeroOrderPolicyTests.cs
+++ SignalsIntegrationTests/SignalsIntegrationTests/ZeroOrderPolicyTests.cs
@@ -6,7 +6,7 @@ using SignalsIntegrationTests.Infrastructure;
 namespace SignalsIntegrationTests
 {
     [TestClass]
-    public class ZeroOrderPolicyTests : MissingValuePolicyTestsBase<int>
+    public class ZeroOrderPolicyTests<T> : MissingValuePolicyTestsBase<T>
     {
         private DateTime BeginTimestamp { get { return new DateTime(2020, 10, 12); } }
 
@@ -17,13 +17,13 @@ namespace SignalsIntegrationTests
         [ClassInitialize]
         public static new void ClassInitialize(TestContext testContext)
         {
-            MissingValuePolicyTestsBase<int>.ClassInitialize(testContext);
+            MissingValuePolicyTestsBase<T>.ClassInitialize(testContext);
         }
 
         [ClassCleanup]
         public static new void ClassCleanup()
         {
-            MissingValuePolicyTestsBase<int>.ClassCleanup();
+            MissingValuePolicyTestsBase<T>.ClassCleanup();
         }
 
         [TestInitialize]
@@ -31,7 +31,7 @@ namespace SignalsIntegrationTests
         {
             GivenASignalWith(Granularity.Day);
 
-            WithMissingValuePolicy(new Domain.MissingValuePolicy.ZeroOrderMissingValuePolicy<int>());
+            WithMissingValuePolicy(new Domain.MissingValuePolicy.ZeroOrderMissingValuePolicy<T>());
         }
 
         [TestMethod]
@@ -42,7 +42,7 @@ namespace SignalsIntegrationTests
 
             WhenReadingData(BeginTimestamp, EndTimestamp);
 
-            ThenResultEquals(DatumArray<int>
+            ThenResultEquals(DatumArray<T>
                 .WithNoneQualityForRange(BeginTimestamp, EndTimestamp, Granularity.Day));
         }
 
@@ -50,24 +50,24 @@ namespace SignalsIntegrationTests
         [TestCategory("issue10")]
         public void GivenSingleDatumAtBeginning_FillsRemainingRangeWithThatValue()
         {
-            GivenSingleDatum(new Datum<int>() { Quality = Quality.Fair, Value = 1410, Timestamp = BeginTimestamp });
+            GivenSingleDatum(new Datum<T>() { Quality = Quality.Fair, Value = Value(Value(1410)), Timestamp = BeginTimestamp });
 
             WhenReadingData(BeginTimestamp, EndTimestamp);
 
-            ThenResultEquals(DatumArray<int>
-                .WithSpecificValueAndQualityForRange(1410, Quality.Fair, BeginTimestamp, EndTimestamp, Granularity.Day));
+            ThenResultEquals(DatumArray<T>
+                .WithSpecificValueAndQualityForRange(Value(1410), Quality.Fair, BeginTimestamp, EndTimestamp, Granularity.Day));
         }
 
         [TestMethod]
         [TestCategory("issue10")]
         public void GivenSingleDatumAfterBeginning_FillsRemainingRangeWithThatValue()
         {
-            GivenSingleDatum(new Datum<int>() { Quality = Quality.Fair, Value = 1410, Timestamp = BeginTimestamp.AddDays(1) });
+            GivenSingleDatum(new Datum<T>() { Quality = Quality.Fair, Value = Value(1410), Timestamp = BeginTimestamp.AddDays(1) });
 
             WhenReadingData(BeginTimestamp, EndTimestamp);
 
-            ThenResultEquals(DatumArray<int>
-                .WithSpecificValueAndQualityForRange(1410, Quality.Fair, BeginTimestamp, EndTimestamp, Granularity.Day)
+            ThenResultEquals(DatumArray<T>
+                .WithSpecificValueAndQualityForRange(Value(1410), Quality.Fair, BeginTimestamp, EndTimestamp, Granularity.Day)
                 .StartingWithNoneQuality());
         }
 
@@ -75,24 +75,24 @@ namespace SignalsIntegrationTests
         [TestCategory("issue10")]
         public void GivenSingleDatumAtTheEnd_ReturnsThatSingleValue()
         {
-            GivenSingleDatum(new Datum<int>() { Quality = Quality.Fair, Value = 1410, Timestamp = EndTimestamp.AddDays(-1) });
+            GivenSingleDatum(new Datum<T>() { Quality = Quality.Fair, Value = Value(1410), Timestamp = EndTimestamp.AddDays(-1) });
 
             WhenReadingData(BeginTimestamp, EndTimestamp);
 
-            ThenResultEquals(DatumArray<int>
+            ThenResultEquals(DatumArray<T>
                 .WithNoneQualityForRange(BeginTimestamp, EndTimestamp, Granularity.Day)
-                .EndingWith(1410, Quality.Fair));
+                .EndingWith(Value(1410), Quality.Fair));
         }
 
         [TestMethod]
         [TestCategory("issue10")]
         public void GivenSingleDatumAfterTheEnd_ReturnsNoneQualitytForRange()
         {
-            GivenSingleDatum(new Datum<int>() { Quality = Quality.Fair, Value = 1410, Timestamp = EndTimestamp });
+            GivenSingleDatum(new Datum<T>() { Quality = Quality.Fair, Value = Value(1410), Timestamp = EndTimestamp });
 
             WhenReadingData(BeginTimestamp, EndTimestamp);
 
-            ThenResultEquals(DatumArray<int>
+            ThenResultEquals(DatumArray<T>
                 .WithNoneQualityForRange(BeginTimestamp, EndTimestamp, Granularity.Day));
         }
 
@@ -100,43 +100,43 @@ namespace SignalsIntegrationTests
         [TestCategory("issue10")]
         public void GivenSingleDatumInTheMiddle_ReturnsNoneQualityBeforeMiddleAndGivenValueAfter()
         {
-            GivenSingleDatum(new Datum<int>() { Quality = Quality.Fair, Value = 1410, Timestamp = MiddleTimestamp });
+            GivenSingleDatum(new Datum<T>() { Quality = Quality.Fair, Value = Value(1410), Timestamp = MiddleTimestamp });
 
             WhenReadingData(BeginTimestamp, EndTimestamp);
 
-            ThenResultEquals(DatumArray<int>
+            ThenResultEquals(DatumArray<T>
                 .WithNoneQualityForRange(BeginTimestamp, MiddleTimestamp, Granularity.Day)
-                .FollowedBy(DatumArray<int>
-                    .WithSpecificValueAndQualityForRange(1410, Quality.Fair, MiddleTimestamp, EndTimestamp, Granularity.Day)));
+                .FollowedBy(DatumArray<T>
+                    .WithSpecificValueAndQualityForRange(Value(1410), Quality.Fair, MiddleTimestamp, EndTimestamp, Granularity.Day)));
         }
 
         [TestMethod]
         [TestCategory("issue10")]
         public void GivenDatumAtBeginningAndInTheMiddle_ReturnsFirstValueBeforeMiddleAndSecondValueAfter()
         {
-            GivenData(new Datum<int>() { Quality = Quality.Poor, Value = 753, Timestamp = BeginTimestamp },
-                      new Datum<int>() { Quality = Quality.Fair, Value = 1410, Timestamp = MiddleTimestamp });
+            GivenData(new Datum<T>() { Quality = Quality.Poor, Value = Value(753), Timestamp = BeginTimestamp },
+                      new Datum<T>() { Quality = Quality.Fair, Value = Value(1410), Timestamp = MiddleTimestamp });
 
             WhenReadingData(BeginTimestamp, EndTimestamp);
 
-            ThenResultEquals(DatumArray<int>
-                .WithSpecificValueAndQualityForRange(753, Quality.Poor, BeginTimestamp, MiddleTimestamp, Granularity.Day)
-                .FollowedBy(DatumArray<int>
-                    .WithSpecificValueAndQualityForRange(1410, Quality.Fair, MiddleTimestamp, EndTimestamp, Granularity.Day)));
+            ThenResultEquals(DatumArray<T>
+                .WithSpecificValueAndQualityForRange(Value(753), Quality.Poor, BeginTimestamp, MiddleTimestamp, Granularity.Day)
+                .FollowedBy(DatumArray<T>
+                    .WithSpecificValueAndQualityForRange(Value(1410), Quality.Fair, MiddleTimestamp, EndTimestamp, Granularity.Day)));
         }
 
         [TestMethod]
         [TestCategory("issue10")]
         public void GivenDatumAtBeginningAndNoneTheMiddle_ReturnsFirstValueBeforeMiddleAndNoneForRestOfRange()
         {
-            GivenData(new Datum<int>() { Quality = Quality.Poor, Value = 753, Timestamp = BeginTimestamp },
-                      new Datum<int>() { Quality = Quality.None, Timestamp = MiddleTimestamp });
+            GivenData(new Datum<T>() { Quality = Quality.Poor, Value = Value(753), Timestamp = BeginTimestamp },
+                      new Datum<T>() { Quality = Quality.None, Timestamp = MiddleTimestamp });
 
             WhenReadingData(BeginTimestamp, EndTimestamp);
 
-            ThenResultEquals(DatumArray<int>
-                .WithSpecificValueAndQualityForRange(753, Quality.Poor, BeginTimestamp, MiddleTimestamp, Granularity.Day)
-                .FollowedBy(DatumArray<int>
+            ThenResultEquals(DatumArray<T>
+                .WithSpecificValueAndQualityForRange(Value(753), Quality.Poor, BeginTimestamp, MiddleTimestamp, Granularity.Day)
+                .FollowedBy(DatumArray<T>
                     .WithNoneQualityForRange(MiddleTimestamp, EndTimestamp, Granularity.Day)));
         }
 
@@ -144,12 +144,92 @@ namespace SignalsIntegrationTests
         [TestCategory("issue10")]
         public void GivenSingleDatumBeforeBeginning_ReturnsItValueForTheWholeRange()
         {
-            GivenSingleDatum(new Datum<int>() { Quality = Quality.Fair, Value = 1410, Timestamp = BeginTimestamp.AddDays(-10) });
+            GivenSingleDatum(new Datum<T>() { Quality = Quality.Fair, Value = Value(1410), Timestamp = BeginTimestamp.AddDays(-10) });
 
             WhenReadingData(BeginTimestamp, EndTimestamp);
 
-            ThenResultEquals(DatumArray<int>
-                .WithSpecificValueAndQualityForRange(1410, Quality.Fair, BeginTimestamp, EndTimestamp, Granularity.Day));
+            ThenResultEquals(DatumArray<T>
+                .WithSpecificValueAndQualityForRange(Value(1410), Quality.Fair, BeginTimestamp, EndTimestamp, Granularity.Day));
+        }
+    }
+
+    [TestClass]
+    public class ZeroOrderPolicyBoolTests : ZeroOrderPolicyTests<bool>
+    {
+        [ClassInitialize]
+        public static new void ClassInitialize(TestContext testContext)
+        {
+            ZeroOrderPolicyTests<bool>.ClassInitialize(testContext);
+        }
+
+        [ClassCleanup]
+        public static new void ClassCleanup()
+        {
+            ZeroOrderPolicyTests<bool>.ClassCleanup();
+        }
+    }
+
+    [TestClass]
+    public class ZeroOrderPolicyIntTests : ZeroOrderPolicyTests<int>
+    {
+        [ClassInitialize]
+        public static new void ClassInitialize(TestContext testContext)
+        {
+            ZeroOrderPolicyTests<int>.ClassInitialize(testContext);
+        }
+
+        [ClassCleanup]
+        public static new void ClassCleanup()
+        {
+            ZeroOrderPolicyTests<int>.ClassCleanup();
+        }
+    }
+
+    [TestClass]
+    public class ZeroOrderPolicyDoubleTests : ZeroOrderPolicyTests<double>
+    {
+        [ClassInitialize]
+        public static new void ClassInitialize(TestContext testContext)
+        {
+            ZeroOrderPolicyTests<double>.ClassInitialize(testContext);
+        }
+
+        [ClassCleanup]
+        public static new void ClassCleanup()
+        {
+            ZeroOrderPolicyTests<double>.ClassCleanup();
+        }
+    }
+
+    [TestClass]
+    public class ZeroOrderPolicyDecimalTests : ZeroOrderPolicyTests<decimal>
+    {
+        [ClassInitialize]
+        public static new void ClassInitialize(TestContext testContext)
+        {
+            ZeroOrderPolicyTests<decimal>.ClassInitialize(testContext);
+        }
+
+        [ClassCleanup]
+        public static new void ClassCleanup()
+        {
+            ZeroOrderPolicyTests<decimal>.ClassCleanup();
+        }
+    }
+
+    [TestClass]
+    public class ZeroOrderPolicyStringTests : ZeroOrderPolicyTests<string>
+    {
+        [ClassInitialize]
+        public static new void ClassInitialize(TestContext testContext)
+        {
+            ZeroOrderPolicyTests<string>.ClassInitialize(testContext);
+        }
+
+        [ClassCleanup]
+        public static new void ClassCleanup()
+        {
+            ZeroOrderPolicyTests<string>.ClassCleanup();
         }
     }
 }
