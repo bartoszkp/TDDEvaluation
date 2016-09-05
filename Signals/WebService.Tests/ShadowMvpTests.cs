369c79269be3f52a074be434d656d528fd565b43
? <?>
2016-09-05 14:47:01 +0200

    GetData_ShadowMvpFillsData() fails

--- Signals/WebService.Tests/ShadowMvpTests.cs
+++ Signals/WebService.Tests/ShadowMvpTests.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccess.GenericInstantiations;
 using Domain;
 using Domain.Exceptions;
+using Domain.MissingValuePolicy;
 using Domain.Repositories;
 using Domain.Services.Implementation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -59,6 +60,58 @@ namespace WebService.Tests
             signalsRepositoryMock.Setup(sr => sr.Get(1)).Returns(returnedSignal);
             signalsWebService.SetMissingValuePolicy(1, mvp);
 
+        }
+
+        [TestMethod]
+        public void GetData_ShadowMvpFillsData()
+        {
+            SetupWebService();
+            var returnedSignal = new Signal()
+            {
+                Id = 1,
+                DataType = DataType.Decimal,
+                Granularity = Granularity.Month
+            };
+
+            var shadowSignal = new Signal()
+            {
+                Id = 2,
+                DataType = DataType.Decimal,
+                Granularity = Granularity.Month
+            };
+
+            var from = new DateTime(1999, 11, 1);
+            var to = new DateTime(2000, 11, 1);
+
+            signalsRepositoryMock.Setup(sr => sr.Get(1)).Returns(returnedSignal);
+            signalsDataRepositoryMock.Setup(sd => sd.GetData<decimal>(returnedSignal, from, to))
+                .Returns(new List<Datum<decimal>>()
+                {
+                    new Datum<decimal>() { Quality = Quality.Good, Timestamp = new DateTime(2000, 1, 1), Value = 1m },
+                    new Datum<decimal>() { Quality = Quality.Good, Timestamp = new DateTime(2000, 5, 1), Value = 2m },
+                    new Datum<decimal>() { Quality = Quality.Good, Timestamp = new DateTime(2000, 8, 1), Value = 5m }
+                });
+
+            signalsDataRepositoryMock.Setup(sd => sd.GetData<decimal>(shadowSignal, from, to))
+                .Returns(new List<Datum<decimal>>()
+                {
+                    new Datum<decimal>() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 3, 1), Value = 1.4m },
+                    new Datum<decimal>() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 5, 1), Value = 0.0m },
+                    new Datum<decimal>() { Quality = Quality.Bad, Timestamp = new DateTime(2000, 9, 1), Value = 7.0m }
+                });
+
+
+
+            missingValuePolicyRepositoryMock.Setup(s => s.Get(returnedSignal))
+                .Returns(new ShadowMissingValuePolicyInteger());
+
+
+            var result = signalsWebService.GetData(1, from, to);
+            var filledDatumForMarch = result.ElementAt(4);
+
+            Assert.AreEqual(12, result.Count());
+            Assert.AreEqual(Dto.Quality.Poor, filledDatumForMarch.Quality);
+            Assert.AreEqual(1.4m, filledDatumForMarch.Value);
         }
 
 
