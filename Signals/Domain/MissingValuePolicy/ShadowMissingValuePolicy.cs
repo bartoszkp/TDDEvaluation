2c53a4deb1d6a89dddb956da9478ca8eb42ccc3a
? <?>
2016-09-05 03:32:37 +0200

    [INFRASTRUCTURE] Added ShadowMissingValuePolicy

---
+++ Signals/Domain/MissingValuePolicy/ShadowMissingValuePolicy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.MissingValuePolicy
+{
+    public class ShadowMissingValuePolicy<T> : MissingValuePolicy<T>
+    {
+        public virtual Signal ShadowSignal { get; set; }
+
+        public override Datum<T> FillMissingValue(Datum<T> datum)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
