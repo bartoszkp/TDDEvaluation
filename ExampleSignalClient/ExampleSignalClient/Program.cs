fd577992f6c1e0a4db49cc66810966721edefbd4
? <?>
2016-09-06 15:04:20 +0200

    Implemented ZeroOrderMissingValuePolicy and Test for client

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -13,17 +13,24 @@ namespace ExampleSignalClient
             {
                 DataType = DataType.Boolean,
                 Granularity = Granularity.Day,
-                Path = new Path() { Components = new[] { "Bool" } }
+                Path = new Path() { Components = new[] { "ZeroOrderTests" } }
             }).Id.Value;
+
+            client.SetMissingValuePolicy(id, new ZeroOrderMissingValuePolicy() { DataType = DataType.Boolean });
 
             client.SetData(id, new Datum[]
             {
-    new Datum() { Quality = Quality.Bad, Timestamp = new DateTime(2000, 1, 1), Value = false }
+    new Datum() { Quality = Quality.Bad, Timestamp = new DateTime(2000, 1, 1), Value = false },
+    new Datum() { Quality = Quality.Bad, Timestamp = new DateTime(2000, 1, 5), Value = true }
             });
 
-            var result = client.GetData(id, new DateTime(2000, 3, 1), new DateTime(2000, 1, 1));
+            var result = client.GetData(id, new DateTime(2000, 1, 1), new DateTime(2000, 1, 10));
 
-            Console.WriteLine(result.Length);
+            foreach (var d in result)
+            {
+                Console.WriteLine(d.Timestamp + ": " + d.Value + " (" + d.Quality + ")");
+            }
+
             Console.ReadKey();
 
         }
