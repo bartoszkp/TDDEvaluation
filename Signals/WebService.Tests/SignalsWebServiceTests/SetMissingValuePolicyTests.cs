b2f2fb3c50e6431c34b50ee1ed462dc53310469c
? <?>
2016-09-06 12:51:23 +0200

    added: tests for shadowValueMVP

--- Signals/WebService.Tests/SignalsWebServiceTests/SetMissingValuePolicyTests.cs
+++ Signals/WebService.Tests/SignalsWebServiceTests/SetMissingValuePolicyTests.cs
@@ -4,6 +4,7 @@ using WebService.Tests.SignalsWebServiceTests.Infrastructure;
 using DataAccess.GenericInstantiations;
 using Dto.MissingValuePolicy;
 using Moq;
+using System.Reflection;
 
 namespace WebService.Tests.SignalsWebServiceTests
 {
@@ -39,6 +40,47 @@ namespace WebService.Tests.SignalsWebServiceTests
             missingValuePolicyRepoMock
                .Verify(mvp => mvp.Set(It.Is<Domain.Signal>(sig => sig.Id == signalId),
                    It.IsAny<Domain.MissingValuePolicy.SpecificValueMissingValuePolicy<double>>()));
-        } 
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TargetInvocationException))]
+        public void GivenASignal_ShadowMissingValuePolicy_WhenSignalsAreDifferent_ExpectedException()
+        {
+            int signalId = 5;
+            Dto.Signal shadowSignal = new Dto.Signal()
+            {
+                DataType = Dto.DataType.Double,
+                Granularity = Dto.Granularity.Minute,
+                Path = new Dto.Path { Components = new[] { "aaa" } }
+            };
+
+            SetupGet(Utils.SignalWith(signalId, Domain.DataType.Double, Domain.Granularity.Hour));
+
+            signalsWebService.SetMissingValuePolicy(
+                signalId,
+                new ShadowMissingValuePolicy {DataType = Dto.DataType.Double, ShadowSignal = shadowSignal });
+        }
+
+        [TestMethod]
+        public void GivenASignal_ShadowMissingValuePolicy_WhenSignalsAreDifferent_CheckIsSetted()
+        {
+            int signalId = 5;
+            Dto.Signal shadowSignal = new Dto.Signal()
+            {
+                DataType = Dto.DataType.Double,
+                Granularity = Dto.Granularity.Minute,
+                Path = new Dto.Path { Components = new[] { "aaa" } }
+            };
+
+            SetupGet(Utils.SignalWith(signalId, Domain.DataType.Double, Domain.Granularity.Minute));
+
+            signalsWebService.SetMissingValuePolicy(
+               signalId,
+               new ShadowMissingValuePolicy { DataType = Dto.DataType.Double, ShadowSignal = shadowSignal });
+
+            missingValuePolicyRepoMock
+             .Verify(mvp => mvp.Set(It.Is<Domain.Signal>(sig => sig.Id == signalId),
+                 It.IsAny<Domain.MissingValuePolicy.ShadowMissingValuePolicy<double>>()));
+        }
     }
 }
