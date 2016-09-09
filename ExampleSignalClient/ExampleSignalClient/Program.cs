aa1451a0dbfc0a9a199af2ff171fcc34b1f52de2
? <?>
2016-09-09 16:10:43 +0200

    Implemented ShadowMissingValuePolicy

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -13,16 +13,30 @@ namespace ExampleSignalClient
             {
                 DataType = DataType.Decimal,
                 Granularity = Granularity.Month,
-                Path = new Path() { Components = new[] { "FirstOrderTests51" } }
+                Path = new Path() { Components = new[] { "ShadowTests" } }
             }).Id.Value;
 
-            client.SetMissingValuePolicy(id, new FirstOrderMissingValuePolicy() { DataType = DataType.Decimal });
+            var shadow = client.Add(new Signal()
+            {
+                DataType = DataType.Decimal,
+                Granularity = Granularity.Month,
+                Path = new Path() { Components = new[] { "shadows", "shadow1" } }
+            });
+
+            client.SetData(shadow.Id.Value, new Datum[]
+            {
+    new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 3, 1), Value = 1.4m },
+    new Datum() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 5, 1), Value = 0.0m },
+    new Datum() { Quality = Quality.Bad, Timestamp = new DateTime(2000, 9, 1), Value = 7.0m }
+            });
+
+            client.SetMissingValuePolicy(id, new ShadowMissingValuePolicy() { DataType = DataType.Decimal, ShadowSignal = shadow });
 
             client.SetData(id, new Datum[]
             {
     new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 1, 1), Value = 1m },
     new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 5, 1), Value = 2m },
-    new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 8, 1), Value = 5m }
+    new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2000, 8, 1), Value = 5m }
             });
 
             var result = client.GetData(id, new DateTime(1999, 11, 1), new DateTime(2000, 11, 1));
--- Signals/Domain/MissingValuePolicy/ShadowMissingValuePolicy.cs
+++ Signals/Domain/MissingValuePolicy/ShadowMissingValuePolicy.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Domain.MissingValuePolicy
 {
@@ -10,7 +11,50 @@ namespace Domain.MissingValuePolicy
 
         public override IEnumerable<Datum<T>> FillData(Signal signal, IEnumerable<Datum<T>> data, DateTime fromIncludedUtc, DateTime toExcludedUtc, ISignalsDataRepository signalsDataRepository)
         {
-            throw new NotImplementedException();
+            List<Domain.Datum<T>> datumSignal = data.ToList<Datum<T>>();
+            var datumShadowSignal = signalsDataRepository.GetData<T>(ShadowSignal, fromIncludedUtc, toExcludedUtc).ToList<Datum<T>>();
+            List<Domain.Datum<T>> datumsFirst = new List<Datum<T>>();
+            while (fromIncludedUtc < toExcludedUtc)
+            {
+                if (datumSignal.Find(x => x.Timestamp == fromIncludedUtc) == null)
+                {
+                    if (datumShadowSignal.Find(x => x.Timestamp == fromIncludedUtc) == null)
+                    {
+                        datumsFirst.Add(new Datum<T>()
+                        {
+                            Quality = Quality.None,
+                            Value = default(T),
+                            Timestamp = fromIncludedUtc
+                        });
+                    }
+                    else
+                    {
+                        datumsFirst.Add(datumShadowSignal.Find(x => x.Timestamp == fromIncludedUtc));
+                    }
+                }
+                else
+                {
+                    datumsFirst.Add(datumSignal.Find(x => x.Timestamp == fromIncludedUtc));
+                }
+                fromIncludedUtc = AddingTimespanToDataTime(fromIncludedUtc, signal.Granularity);
+            }
+            return datumsFirst;
+        }
+
+        private DateTime AddingTimespanToDataTime(DateTime addedData, Granularity granularitySignal)
+        {
+            var checkGranularity = new Dictionary<Granularity, Action>()
+            {
+                {Granularity.Day, ()=> addedData=addedData.AddDays(1) },
+                {Granularity.Hour, ()=> addedData=addedData.AddHours(1) },
+                {Granularity.Minute, ()=> addedData=addedData.AddMinutes(1) },
+                {Granularity.Month, ()=> addedData=addedData.AddMonths(1) },
+                {Granularity.Second, ()=> addedData=addedData.AddSeconds(1) },
+                {Granularity.Week, ()=> addedData=addedData.AddDays(7) },
+                {Granularity.Year, ()=> addedData=addedData.AddYears(1) }
+            };
+            checkGranularity[granularitySignal].Invoke();
+            return addedData;
         }
     }
 }
