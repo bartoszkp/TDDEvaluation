8d8fd0fe63319dab2a0b095c5f80a85083706231
? <?>
2016-09-12 12:28:37 +0200

    Test add: Setting ShadowMissingValuePolicy should not create a dependency cycle (Issue #25)

--- Signals/WebService.Tests/SignalsWebServiceTests/SetMissingValuePolicyTests.cs
+++ Signals/WebService.Tests/SignalsWebServiceTests/SetMissingValuePolicyTests.cs
@@ -85,5 +85,56 @@ namespace WebService.Tests.SignalsWebServiceTests
              .Verify(mvp => mvp.Set(It.Is<Domain.Signal>(sig => sig.Id == signalId),
                  It.IsAny<Domain.MissingValuePolicy.ShadowMissingValuePolicy<double>>()));
         }
+        [TestMethod]
+        [ExpectedException(typeof(TargetInvocationException))]
+        public void GivenASignal_ShadowMissingValuePolicy_WhenSignalsMakeCycle_CheckIsSetted()
+        {
+            int signalId1 = 1;
+            int signalId2 = 2;
+            int signalId3 = 3;
+            Dto.Signal signal1 = new Dto.Signal()
+            {
+                DataType = Dto.DataType.Double,
+                Granularity = Dto.Granularity.Minute,
+                Path = new Dto.Path { Components = new[] { "aaa" } },
+                Id = signalId1
+            };
+            Domain.Signal signal2 = new Domain.Signal()
+            {
+                DataType =Domain.DataType.Double,
+                Granularity = Domain.Granularity.Minute,
+     
+                Id = signalId2
+
+            };
+            Domain.Signal signal3 = new Domain.Signal()
+            {
+                DataType = Domain.DataType.Double,
+                Granularity = Domain.Granularity.Minute,
+                Id = signalId3
+
+            };
+
+            SetupGet(Utils.SignalWith(signalId1, Domain.DataType.Double, Domain.Granularity.Minute));
+            SetupGet(Utils.SignalWith(signalId2, Domain.DataType.Double, Domain.Granularity.Minute));
+            SetupGet(Utils.SignalWith(signalId3, Domain.DataType.Double, Domain.Granularity.Minute));
+
+            Domain.MissingValuePolicy.ShadowMissingValuePolicy<double> missingValuePolicy1 = new Domain.MissingValuePolicy.ShadowMissingValuePolicy<double>() { Id = signalId1, ShadowSignal = signal2 };
+
+            missingValuePolicyRepoMock
+              .Setup(mvp => mvp.Get(It.Is<Domain.Signal>(x=>x.Id==signalId1)))
+              .Returns(missingValuePolicy1);
+
+            Domain.MissingValuePolicy.ShadowMissingValuePolicy<double> missingValuePolicy2 = new Domain.MissingValuePolicy.ShadowMissingValuePolicy<double>() { Id = signalId2, ShadowSignal = signal3 };
+
+            missingValuePolicyRepoMock
+              .Setup(mvp => mvp.Get(It.Is<Domain.Signal>(x => x.Id == signalId2)))
+              .Returns(missingValuePolicy2);
+
+            signalsWebService.SetMissingValuePolicy(
+              signalId3,
+              new ShadowMissingValuePolicy { DataType = Dto.DataType.Double, ShadowSignal = signal1, Id = signalId3 });
+
+        }
     }
 }
