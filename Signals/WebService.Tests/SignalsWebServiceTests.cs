4be18d1113565d8a39adc3262c3926eb7fcce267
? <?>
2016-09-12 11:27:22 +0200

    Added GivenASignal_WhenGettingCoarseDataWithEqualFromAndToTimeStamps_ReturnsOneDatumWithWorstQualityFromRange unit test

--- Signals/WebService.Tests/SignalsWebServiceTests.cs
+++ Signals/WebService.Tests/SignalsWebServiceTests.cs
@@ -1905,6 +1905,37 @@ namespace WebService.Tests
                 Assert.AreEqual(3, result.Single().Value);
             }
 
+            [TestMethod]
+            public void GivenASignal_WhenGettingCoarseDataWithEqualFromAndToTimeStamps_ReturnsOneDatumWithWorstQualityFromRange()
+            {
+                SetupWebService();
+                int id = 6;
+
+                DateTime dateFrom = new DateTime(2000, 5, 1);
+                DateTime dateTo = new DateTime(2000, 5, 1);
+                DateTime realDateTo = new DateTime(2000, 5, 8);
+
+                var signal = SignalWith(DataType.Integer, Granularity.Day, Path.FromString("root/signalInt"), id);
+
+                var dataReturned = new Domain.Datum<int>[]
+                {
+                    new Datum<int>() { Quality = Quality.Good, Timestamp = new DateTime(2000, 5, 1), Value = (int)2 },
+                    new Datum<int>() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 5, 2), Value = (int)1 },
+                    new Datum<int>() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 5, 3), Value = (int)3 },
+                    new Datum<int>() { Quality = Quality.Bad, Timestamp = new DateTime(2000, 5, 4), Value = (int)5 },
+                    new Datum<int>() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 5, 5), Value = (int)3 },
+                    new Datum<int>() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 5, 6), Value = (int)3 },
+                    new Datum<int>() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 5, 7), Value = (int)4 },
+                };
+
+                SetupRepositoryMocks_GetData_ReturnsData_WithinTime<int>(id, signal, dateFrom, realDateTo, dataReturned);
+
+                var result = signalsWebService.GetCoarseData(id, Dto.Granularity.Week, dateFrom, dateTo);
+
+                Assert.AreEqual(1, result.Count());
+                Assert.AreEqual(Dto.Quality.Bad, result.Single().Quality);
+            }
+
             private void DeleteASignal(int id)
             {
 
