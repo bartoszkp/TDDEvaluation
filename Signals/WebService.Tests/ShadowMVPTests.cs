30f8e4bc5d70ca6f58e90ea177158c5c3ad909af
? <?>
2016-09-12 14:32:38 +0200

    ShadowMVPCorrectlyFillMissingData fail

--- Signals/WebService.Tests/ShadowMVPTests.cs
+++ Signals/WebService.Tests/ShadowMVPTests.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Repositories;
+﻿using Domain;
+using Domain.Repositories;
 using Domain.Services.Implementation;
 using Dto.MissingValuePolicy;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -72,6 +73,74 @@ namespace WebService.Tests
             signalsWebService.SetMissingValuePolicy(signal.Id.Value, shadowMVP);
         }
 
+        [TestMethod]
+        public void ShadowMVPCorrectlyFillMissingData()
+        {
+            var signal = new Signal()
+            {
+                Id = 904,
+                DataType = DataType.Decimal,
+                Granularity = Granularity.Month,
+            };
+
+            var shadowSignal = new Signal()
+            {
+                Id = 2435,
+                DataType = DataType.Decimal,
+                Granularity = Granularity.Month,
+            };
+
+            var datums = new Datum<decimal>[]
+            {
+                new Datum<decimal>() { Quality = Quality.Good, Timestamp = new DateTime(2000, 1, 1), Value = 1m },
+                new Datum<decimal>() { Quality = Quality.Good, Timestamp = new DateTime(2000, 5, 1), Value = 2m },
+                new Datum<decimal>() { Quality = Quality.Good, Timestamp = new DateTime(2000, 8, 1), Value = 5m }
+            };
+            
+            var shadowDatums = new Datum<decimal>[]
+            {
+                new Datum<decimal>() { Quality = Quality.Fair, Timestamp = new DateTime(2000, 3, 1), Value = 1.4m },
+                new Datum<decimal>() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 5, 1), Value = 0.0m },
+                new Datum<decimal>() { Quality = Quality.Bad, Timestamp = new DateTime(2000, 9, 1), Value = 7.0m }
+            };
+
+            setupWebService();
+            setupGet(signal);
+            setupGet(shadowSignal);
+            setupGetData(signal, datums);
+            setupGetData(shadowSignal, shadowDatums);
+
+            missingValuePolicyRepositoryMock
+                .Setup(mvpr => mvpr.Get(It.Is<Signal>(s => s.Id == signal.Id)))
+                .Returns(new DataAccess.GenericInstantiations.ShadowMissingValuePolicyDecimal() { ShadowSignal = shadowSignal });
+
+            missingValuePolicyRepositoryMock
+                .Setup(mvpr => mvpr.Get(It.Is<Signal>(s => s.Id == shadowSignal.Id)))
+                .Returns(new DataAccess.GenericInstantiations.NoneQualityMissingValuePolicyDecimal());
+
+            var result = signalsWebService.GetData(signal.Id.Value, new DateTime(2000, 1, 1), new DateTime(2000, 9, 1)).ToArray();
+
+            var correctData = new Dto.Datum[]
+            {
+                new Dto.Datum() { Quality = Dto.Quality.Good, Timestamp = new DateTime(2000, 1, 1), Value = 1m },
+                new Dto.Datum() { Quality = Dto.Quality.None, Timestamp = new DateTime(2000, 2, 1), Value = default(decimal) },
+                new Dto.Datum() { Quality = Dto.Quality.Fair, Timestamp = new DateTime(2000, 3, 1), Value = 1.4m },
+                new Dto.Datum() { Quality = Dto.Quality.None, Timestamp = new DateTime(2000, 4, 1), Value = default(decimal) },
+                new Dto.Datum() { Quality = Dto.Quality.Good, Timestamp = new DateTime(2000, 5, 1), Value = 2m },
+                new Dto.Datum() { Quality = Dto.Quality.None, Timestamp = new DateTime(2000, 6, 1), Value = default(decimal) },
+                new Dto.Datum() { Quality = Dto.Quality.None, Timestamp = new DateTime(2000, 7, 1), Value = default(decimal) },
+                new Dto.Datum() { Quality = Dto.Quality.Good, Timestamp = new DateTime(2000, 8, 1), Value = 5m },
+                new Dto.Datum() { Quality = Dto.Quality.Bad, Timestamp = new DateTime(2000, 9, 1), Value = 7.0m }
+            };
+
+            for (int i=0; i<result.Length; ++i)
+            {
+                Assert.AreEqual(correctData[i].Quality, result[i].Quality);
+                Assert.AreEqual(correctData[i].Timestamp, result[i].Timestamp);
+                Assert.AreEqual(correctData[i].Value, result[i].Value);
+            }
+        }
+
         private void setupWebService()
         {
             var signalsDomainService = new SignalsDomainService(
@@ -86,6 +155,16 @@ namespace WebService.Tests
                 .Returns(signal);
         }
 
+        private void setupGetData<T>(Signal signal, Datum<T>[] datums)
+        {
+            signalsDataRepositoryMock
+                .Setup(sdr => sdr.GetData<T>(
+                    It.Is<Signal>(s => s.Id == signal.Id),
+                    It.IsAny<DateTime>(),
+                    It.IsAny<DateTime>()))
+                .Returns(datums);
+        }
+        
         private Mock<ISignalsRepository> signalsRepositoryMock = new Mock<ISignalsRepository>();
         private Mock<IMissingValuePolicyRepository> missingValuePolicyRepositoryMock = new Mock<IMissingValuePolicyRepository>();
         private Mock<ISignalsDataRepository> signalsDataRepositoryMock = new Mock<ISignalsDataRepository>();
