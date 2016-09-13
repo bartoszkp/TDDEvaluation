bc325c15a12d833889740e189138202132bf100e
? <?>
2016-09-13 12:04:55 +0200

    client example code

--- ExampleSignalClient/ExampleSignalClient/Program.cs
+++ ExampleSignalClient/ExampleSignalClient/Program.cs
@@ -9,27 +9,49 @@ namespace ExampleSignalClient
         {
             SignalsWebServiceClient client = new SignalsWebServiceClient("BasicHttpBinding_ISignalsWebService");
 
-            var id = client.Add(new Signal() { Granularity = Granularity.Day, Path = new Path() { Components = new[] { String.Empty } } }).Id.Value;
-
-            var result = client.GetById(345);
-
-            if (result != null)
+            var id = client.Add(new Signal()
             {
-                Console.WriteLine("Sygnał poprawnie utworzony");
-            }
-            else
-            {
-                Console.WriteLine("Błąd - nie udało się utworzyć sygnału");
-            }
+                DataType = DataType.Integer,
+                Granularity = Granularity.Day,
+                Path = new Path() { Components = new[] { "CoarseTests" } }
+            }).Id.Value;
 
-            var data = client.GetData(id, new DateTime(2018, 12, 12), new DateTime(2018, 12, 12));
-
-            foreach (var datum in data)
+            client.SetData(id, new Datum[]
             {
-                Console.WriteLine("Datum: " + datum.Quality + " " + datum.Timestamp);
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1, 4), Value = 1 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1, 5), Value = 1 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1, 6), Value = 1 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1, 7), Value = 1 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1, 8), Value = 1 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1, 9), Value = 1 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1,10), Value = 1 },
+
+                new Datum() { Quality = Quality.Good, Timestamp = new DateTime(2016,1,11), Value = 5 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1,12), Value = 5 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1,13), Value = 5 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1,14), Value = 5 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1,15), Value = 5 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1,16), Value = 2 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1,17), Value = 1 },
+
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1,18), Value = 5 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1,19), Value = 5 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1,20), Value = 5 },
+                new Datum() { Quality = Quality.Bad,  Timestamp = new DateTime(2016,1,21), Value = 5 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1,22), Value = 0 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1,23), Value = 1 },
+                new Datum() { Quality = Quality.Fair, Timestamp = new DateTime(2016,1,24), Value = 0 },
+            });
+
+            var result = client.GetCoarseData(id, Granularity.Week, new DateTime(2016, 1, 4), new DateTime(2016, 1, 25));
+
+            foreach (var d in result)
+            {
+                Console.WriteLine(d.Timestamp + ": " + d.Value + " (" + d.Quality + ")");
             }
 
             Console.ReadKey();
+
         }
     }
 }
