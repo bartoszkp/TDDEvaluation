04e965195a66a0b709eeec60fc8fa9872bf34582
? <?>
2016-09-13 11:46:27 +0200

    GetData(ts,ts) tests implementation

--- Signals/WebService.Tests/GetDataTests.cs
+++ Signals/WebService.Tests/GetDataTests.cs
@@ -58,6 +58,135 @@ namespace WebService.Tests
 
             Assert.IsTrue(result.ElementAt(0).Timestamp < result.ElementAt(1).Timestamp);
         }
+        [TestMethod]
+        public void WhenGetData_WithSameTime_ReturnData_FirsOrder()
+        {
+            SetupWebService();
+
+            var signal = new Domain.Signal()
+            {
+                DataType = DataType.Double,
+                Granularity = Granularity.Second,
+                Id = 1,
+                Path = Path.FromString("x/z")
+            };
+
+            var time = new DateTime(2000, 1, 1);
+
+            Datum<double>[] data =
+             {
+                new Datum<double>() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 1, 1), Value = (double)1.5 }
+            };
+
+            signalsRepositoryMock.Setup(x => x.Get(1)).Returns(signal);
+
+            signalsDataRepositoryMock.Setup(x => x.GetData<double>(signal, It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+                .Returns(data);
+
+            missingValuePolicyRepositoryMock.Setup(x => x.Get(signal)).Returns(new DataAccess.GenericInstantiations.FirstOrderMissingValuePolicyDouble());
+
+            var returnedData = signalsWebService.GetData(1, time, time);
+
+            Assert.AreEqual(1, returnedData.Count());
+        }
+
+        [TestMethod]
+        public void WhenGetData_WithSameTime_ReturnData_Zero()
+        {
+            SetupWebService();
+
+            var signal = new Domain.Signal()
+            {
+                DataType = DataType.Double,
+                Granularity = Granularity.Second,
+                Id = 1,
+                Path = Path.FromString("x/z")
+            };
+
+            var time = new DateTime(2000, 1, 1);
+
+            Datum<double>[] data =
+             {
+                new Datum<double>() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 1, 1), Value = (double)1.5 }
+            };
+
+            signalsRepositoryMock.Setup(x => x.Get(1)).Returns(signal);
+
+            signalsDataRepositoryMock.Setup(x => x.GetData<double>(signal, It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+                .Returns(data);
+
+            missingValuePolicyRepositoryMock.Setup(x => x.Get(signal)).Returns(new DataAccess.GenericInstantiations.ZeroOrderMissingValuePolicyDouble());
+
+            var returnedData = signalsWebService.GetData(1, time, time);
+
+            Assert.AreEqual(1, returnedData.Count());
+        }
+
+
+        [TestMethod]
+        public void WhenGetData_WithSameTime_ReturnData_None()
+        {
+            SetupWebService();
+
+            var signal = new Domain.Signal()
+            {
+                DataType = DataType.Double,
+                Granularity = Granularity.Second,
+                Id = 1,
+                Path = Path.FromString("x/z")
+            };
+
+            var time = new DateTime(2000, 1, 1);
+
+            Datum<double>[] data =
+             {
+                new Datum<double>() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 1, 1), Value = (double)1.5 }
+            };
+
+            signalsRepositoryMock.Setup(x => x.Get(1)).Returns(signal);
+
+            signalsDataRepositoryMock.Setup(x => x.GetData<double>(signal, It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+                .Returns(data);
+
+            missingValuePolicyRepositoryMock.Setup(x => x.Get(signal)).Returns(new DataAccess.GenericInstantiations.NoneQualityMissingValuePolicyDouble());
+
+            var returnedData = signalsWebService.GetData(1, time, time);
+
+            Assert.AreEqual(1, returnedData.Count());
+        }
+
+        [TestMethod]
+        public void WhenGetData_WithSameTime_ReturnData_Specific()
+        {
+            SetupWebService();
+
+            var signal = new Domain.Signal()
+            {
+                DataType = DataType.Double,
+                Granularity = Granularity.Second,
+                Id = 1,
+                Path = Path.FromString("x/z")
+            };
+
+            var time = new DateTime(2000, 1, 1);
+
+            Datum<double>[] data =
+             {
+                new Datum<double>() { Quality = Quality.Poor, Timestamp = new DateTime(2000, 1, 1), Value = (double)1.5 }
+            };
+
+            signalsRepositoryMock.Setup(x => x.Get(1)).Returns(signal);
+
+            signalsDataRepositoryMock.Setup(x => x.GetData<double>(signal, It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+                .Returns(data);
+
+            missingValuePolicyRepositoryMock.Setup(x => x.Get(signal)).Returns(new DataAccess.GenericInstantiations.SpecificValueMissingValuePolicyDouble());
+
+            var returnedData = signalsWebService.GetData(1, time, time);
+
+            Assert.AreEqual(1, returnedData.Count());
+        }
+
 
         private void SetupWebService()
         {
